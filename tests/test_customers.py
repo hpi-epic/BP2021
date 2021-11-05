64acdab44ec16038f4d24abcfbc31526456111e9
NikkelM <57323886+NikkelM@users.noreply.github.com>
2021-11-05 17:52:35 +0100

    Fix test

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -4,4 +4,4 @@ ident_state = [15, 20, 15, 20]
 
 # Check that Customers only choose actions between 0 and 2
 def test_customer_action_range():
-    assert 5 <= Customer.buy_object(ident_state) <= 5
+    assert 0 <= Customer.buy_object(ident_state) <= 2
