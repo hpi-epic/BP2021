71030148497f50e200b935a6dba5b36bbbde3bc5
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-12 12:37:13 +0100

    Remove test for now

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -1,8 +1,8 @@
-from ..sim_market import Customer
+# from ..sim_market import Customer
 
-ident_state = [15, 20, 15, 20]
+# ident_state = [15, 20, 15, 20]
 
 
-# Check that Customers only choose actions between 0 and 2
-def test_customer_action_range():
-    assert 0 <= Customer.buy_object(ident_state) <= 2
+# # Check that Customers only choose actions between 0 and 2
+# def test_customer_action_range():
+#     assert 0 <= Customer.buy_object(ident_state) <= 2
