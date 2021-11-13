067dc930f44b047fbe150a8015fd1d0b07a7d41b
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-13 14:36:14 +0100

    Added additional formatting exception for context.py

--- src/tests/context.py
+++ src/tests/context.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
+# inserts the current pyth to sys.path to allow for importing from src directory
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 # import any classes needed in tests here and call them as such:
 # from .context import Customer
 from customer import Customer
 from sim_market import SimMarket
-
-# inserts the current pyth to sys.path to allow for importing from src directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
