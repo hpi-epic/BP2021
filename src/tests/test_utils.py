834ed36f8cc9312176ce80c82cf31bd3de3317e1
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-16 10:08:52 +0100

    Added config.json mock test

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,35 +1,41 @@
-# import json
-# from unittest.mock import MagicMock, patch
+import os
+from importlib import reload
+from unittest.mock import mock_open, patch
 
 from .context import utils
 
-# import pytest
-
-
-# WIP: https://stackoverflow.com/questions/51138834/how-to-use-mock-open-with-json-load
-
-# Perhaps take a look at: https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth ?
+# Than you!!11elf: https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth ?
 
 
 def test_reading_file_values():
-    # p1 = patch('builtins.open', MagicMock())
+    with patch(
+        'builtins.open',
+        mock_open(
+            read_data='''{"episode_size": 20,
+                "learning_rate": 1e-6,
+                "max_price": 15,
+                "max_quality": 100,
+                "production_price": 25,
+                "number_of_customers": 30}'''
+        ),
+    ) as mock_file:
+        assert (
+            open(
+                os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
+            ).read()
+            == '''{"episode_size": 20,
+                "learning_rate": 1e-6,
+                "max_price": 15,
+                "max_quality": 100,
+                "production_price": 25,
+                "number_of_customers": 30}'''
+        )
+        mock_file.assert_called_with(
+            os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
+        )
+        utils.config = utils.load_config(
+            os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
+        )
+        reload(utils)
 
-    # m = MagicMock(
-    # 		side_effect=[
-    # 				{
-    # 						'episode_size': 40,
-    # 						'learning_rate': 1e-5,
-    # 						'max_price': 20,
-    # 						'max_quality': 100,
-    # 						'production_price': 10,
-    # 						'number_of_customers': 20,
-    # 				}
-    # 		]
-    # )
-    # p2 = patch('json.load', m)
-
-    # with p1 as p_open:
-    # 	with p2 as p_json_load:
-    # 		config_file = open('config.json')
-    # 		print(json.load(config_file))
-    assert utils.MAX_QUALITY == 100
+        assert utils.MAX_QUALITY == 100
