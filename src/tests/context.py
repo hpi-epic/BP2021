58d46ee2363d117aaacad350e407997a6c63ba06
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-16 13:57:51 +0100

    Add explicit numpy version

--- src/tests/context.py
+++ src/tests/context.py
@@ -5,18 +5,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-# import agent
-# import competitor
-# import customer
-# import exampleprinter
-# import experience_buffer
-# import human_player
-# import model_remover
-# import model
-import sim_market
-# import training
-import utils
-from customer import *
 # import any classes needed in tests here and call them as such:
 # from .context import customer
+from customer import *
 from sim_market import *
