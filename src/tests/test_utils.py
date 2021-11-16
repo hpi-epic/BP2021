bfbbbbac0d9165b65101a83e413649dd3bf2a380
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-16 11:17:30 +0100

    Added create_mock_json function

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -4,31 +4,30 @@ from unittest.mock import mock_open, patch
 
 from .context import utils
 
+
+def create_mock_json(episode_size='20', learning_rate='1e-6', max_price='15', max_quality='100', number_of_customers='30', production_price='5'):
+	return '{\n\t"episode_size": ' + episode_size + ',\n' + \
+		'\t"learning_rate": ' + learning_rate + ',\n' + \
+		'\t"max_price": ' + max_price + ',\n' + \
+		'\t"max_quality": ' + max_quality + ',\n' + \
+		'\t"number_of_customers": ' + number_of_customers + ',\n' + \
+		'\t"production_price": ' + production_price + '\n}'
+
+
 # Thank you!!11elf: https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth
 
-
 def test_reading_file_values():
+	print(create_mock_json())
 	with patch(
 		'builtins.open',
 		mock_open(
-			read_data='''{"episode_size": 20,
-				"learning_rate": 1e-6,
-				"max_price": 15,
-				"max_quality": 100,
-				"number_of_customers": 30,
-				"production_price": 5}'''
-		),
+			read_data=create_mock_json()),
 	) as mock_file:
 		assert(
 			open(
 				os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
 			).read()
-			== '''{"episode_size": 20,
-				"learning_rate": 1e-6,
-				"max_price": 15,
-				"max_quality": 100,
-				"number_of_customers": 30,
-				"production_price": 5}'''
+			== create_mock_json()
 		)
 		# check that the mock_file is read correctly when opening any file
 		mock_file.assert_called_with(
@@ -52,24 +51,13 @@ def test_reading_file_values():
 	with patch(
 		'builtins.open',
 		mock_open(
-			read_data='''{"episode_size": 50,
-				"learning_rate": 1e-4,
-				"max_price": 50,
-				"max_quality": 80,
-				"number_of_customers": 20,
-				"production_price": 10}'''
+			read_data=create_mock_json('50', '1e-4', '50', '80', '20', '10')
 		),
 	) as mock_file:
 		assert (
 			open(
 				os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
-			).read()
-			== '''{"episode_size": 50,
-				"learning_rate": 1e-4,
-				"max_price": 50,
-				"max_quality": 80,
-				"number_of_customers": 20,
-				"production_price": 10}'''
+			).read() == create_mock_json('50', '1e-4', '50', '80', '20', '10')
 		)
 		mock_file.assert_called_with(
 			os.path.dirname(__file__) + os.sep + '...' + os.sep + 'config.json'
