22387ec81cd59da426bfaf91a3d11e6ca9784ee8
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-17 17:12:38 +0100

    Added comments/indendation for better readability

--- src/tests/test_customers.py
+++ src/tests/test_customers.py
@@ -7,7 +7,8 @@ from .context import CustomerLinear as CLinear
 from .context import MultiCompetitorScenario as SMulti
 
 
-def random_state(market_scenario):
+# Helper function that creates a random offer (state that includes the agent's price) to test customer behaviour
+def random_offer(market_scenario):
 	ins = market_scenario()
 	ins.reset()
 	return ins.full_view(random.randint(1, 29))
@@ -15,7 +16,7 @@ def random_state(market_scenario):
 
 # mark.parametrize can be used to run the same test with different parameters
 # Test the LinearCustomer in the different Market Scenarios
-@pytest.mark.parametrize('offers, expectedSize', [(random_state(SClassic), 4), (random_state(SMulti), 8)])
+@pytest.mark.parametrize('offers, expectedSize', [(random_offer(SClassic), 4), (random_offer(SMulti), 8)])
 def test_linear_customer_action_range(offers, expectedSize):
-	assert offers.size == expectedSize
+	assert len(offers) == expectedSize
 	assert 0 <= CLinear.buy_object(CLinear, offers) <= expectedSize - 1
