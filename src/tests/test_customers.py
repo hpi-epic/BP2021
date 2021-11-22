a48de1326b577c2a8872ddf373be3504988fd256
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-11-22 14:04:51 +0100

    Add ids for better readability, reorder utils-tests alphabetically

--- src/tests/test_customers.py
+++ src/tests/test_customers.py
@@ -7,16 +7,21 @@ from .context import CustomerLinear as CLinear
 from .context import MultiCompetitorScenario as SMulti
 
 
-# Helper function that creates a random offer (state that includes the agent's price) to test customer behaviour
+# Helper function that creates a random offer (state that includes the agent's price) to test customer behaviour. This is dependent on the sim_market working!
 def random_offer(market_scenario):
 	ins = market_scenario()
 	ins.reset()
 	return ins.generate_offer(random.randint(1, 29))
 
+def get_linear_customer_action_range_ids():
+	return [
+		'ClassicScenario', 'MultiCompetitorScenario'
+	]
+
 
 # mark.parametrize can be used to run the same test with different parameters
 # Test the LinearCustomer in the different Market Scenarios
-@pytest.mark.parametrize('offers, expectedSize', [(random_offer(SClassic), 4), (random_offer(SMulti), 8)])
+@pytest.mark.parametrize('offers, expectedSize', [(random_offer(SClassic), 4), (random_offer(SMulti), 8)], ids = get_linear_customer_action_range_ids())
 def test_linear_customer_action_range(offers, expectedSize):
 	assert len(offers) == expectedSize
 	buy_decisions = CLinear.buy_object(CLinear, offers)
--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -34,6 +34,49 @@ def check_mock_file(mock_file, json=create_mock_json()):
 	utils.config = utils.load_config(path)
 
 
+# The following variables are input mock-json strings for the test_invalid_values test
+# These tests have invalid values in their input file, the import should throw a specific error message
+odd_number_of_customers = (create_mock_json('50', '1e-4', '50', '80', '21', '10'), 'number_of_customers should be even and positive')
+negative_number_of_customers = (create_mock_json('50', '1e-4', '50', '80', '-10', '10'), 'number_of_customers should be even and positive')
+learning_rate_larger_one = (create_mock_json('50', '1.5', '50', '80', '20', '10'), 'learning_rate should be between 0 and 1 (excluded)')
+neg_learning_rate = (create_mock_json('50', '0', '50', '80', '20', '10'), 'learning_rate should be between 0 and 1 (excluded)')
+prod_price_higher_max_price = (create_mock_json('50', '1e-5', '10', '80', '20', '50'), 'production_price needs to smaller than max_price and positive or zero')
+neg_prod_price = (create_mock_json('50', '1e-5', '50', '80', '20', '-10'), 'production_price needs to smaller than max_price and positive or zero')
+neg_max_quality = (create_mock_json('20', '1e-6', '15', '-80', '30', '5'), 'max_quality should be positive')
+
+# These tests are missing a line in the config file, the import should throw a specific error message
+missing_episode_size = (create_mock_json_with_missing_line(0), 'your config is missing episode_size')
+missing_learning_rate = (create_mock_json_with_missing_line(1), 'your config is missing learning_rate')
+missing_max_price = (create_mock_json_with_missing_line(2), 'your config is missing max_price')
+missing_max_quality = (create_mock_json_with_missing_line(3), 'your config is missing max_quality')
+missing_number_of_customers = (create_mock_json_with_missing_line(4), 'your config is missing number_of_customers')
+missing_prod_price = (create_mock_json_with_missing_line(5), 'your config is missing production_price')
+
+# All pairs concerning themselves with invalid config.json values should be added to this array to get tested in test_invalid_values
+array_testing = [
+	odd_number_of_customers, negative_number_of_customers, learning_rate_larger_one, neg_learning_rate, prod_price_higher_max_price, neg_prod_price, neg_max_quality,
+	missing_episode_size, missing_learning_rate, missing_max_price, missing_max_quality, missing_number_of_customers, missing_prod_price
+]
+
+# This defines how the tests are named. Usually they would be "test_invalid_values[whole_json_here]". This ensure they are named after the actual thing they are testing
+def get_invalid_test_ids():
+	return [
+		'odd_number_of_customers', 'negative_number_of_customers', 'learning_rate_larger_one', 'neg_learning_rate', 'prod_price_higher_max_price', 'neg_prod_price', 'neg_max_quality',
+		'missing_episode_size', 'missing_learning_rate', 'missing_max_price', 'missing_max_quality', 'missing_number_of_customers', 'missing_prod_price'
+	]
+
+
+# Test that checks that an invalid/broken config.json gets detected correctly
+@pytest.mark.parametrize('json_values, expected_error_msg', array_testing, ids = get_invalid_test_ids())
+def test_invalid_values(json_values, expected_error_msg):
+	json = json_values
+	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
+		check_mock_file(mock_file, json)
+		with pytest.raises(AssertionError) as assertion_info:
+			reload(utils)
+		assert expected_error_msg in str(assertion_info.value)
+
+
 # mock format taken from: https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth
 # Test that checks if the config.json is read correctly
 def test_reading_file_values():
@@ -64,39 +107,3 @@ def test_reading_file_values():
 		assert utils.MAX_QUALITY == 80
 		assert utils.NUMBER_OF_CUSTOMERS == 20
 		assert utils.PRODUCTION_PRICE == 10
-
-
-# The following variables are input mock-json strings for the test_invalid_values test
-# These tests have invalid values in their input file, the import should throw a specific error message
-odd_number_of_customers = (create_mock_json('50', '1e-4', '50', '80', '21', '10'), 'number_of_customers should be even and positive')
-negative_number_of_customers = (create_mock_json('50', '1e-4', '50', '80', '-10', '10'), 'number_of_customers should be even and positive')
-learning_rate_larger_one = (create_mock_json('50', '1.5', '50', '80', '20', '10'), 'learning_rate should be between 0 and 1 (excluded)')
-neg_learning_rate = (create_mock_json('50', '0', '50', '80', '20', '10'), 'learning_rate should be between 0 and 1 (excluded)')
-prod_price_higher_max_price = (create_mock_json('50', '1e-5', '10', '80', '20', '50'), 'production_price needs to smaller than max_price and positive or zero')
-neg_prod_price = (create_mock_json('50', '1e-5', '50', '80', '20', '-10'), 'production_price needs to smaller than max_price and positive or zero')
-neg_max_quality = (create_mock_json('20', '1e-6', '15', '-80', '30', '5'), 'max_quality should be positive')
-
-# These tests are missing a line in the config file, the import should throw a specific error message
-missing_episode_size = (create_mock_json_with_missing_line(0), 'your config is missing episode_size')
-missing_learning_rate = (create_mock_json_with_missing_line(1), 'your config is missing learning_rate')
-missing_max_price = (create_mock_json_with_missing_line(2), 'your config is missing max_price')
-missing_max_quality = (create_mock_json_with_missing_line(3), 'your config is missing max_quality')
-missing_number_of_customers = (create_mock_json_with_missing_line(4), 'your config is missing number_of_customers')
-missing_prod_price = (create_mock_json_with_missing_line(5), 'your config is missing production_price')
-
-# All pairs concerning themselves with invalid config.json values should be added to this array to get tested in test_invalid_values
-array_testing = [
-	odd_number_of_customers, negative_number_of_customers, learning_rate_larger_one, neg_learning_rate, prod_price_higher_max_price, neg_prod_price, neg_max_quality,
-	missing_episode_size, missing_learning_rate, missing_max_price, missing_max_quality, missing_number_of_customers, missing_prod_price
-]
-
-
-# Test that checks that an invalid/broken config.json gets detected correctly
-@pytest.mark.parametrize('json_values, expected_error_msg', array_testing)
-def test_invalid_values(json_values, expected_error_msg):
-	json = json_values
-	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
-		check_mock_file(mock_file, json)
-		with pytest.raises(AssertionError) as assertion_info:
-			reload(utils)
-		assert expected_error_msg in str(assertion_info.value)
