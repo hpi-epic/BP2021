ea273897d457276f8ae999f522708bb597c8ddc5
Blackjack <johann.schulze.tast@t-online.de>
2021-11-22 17:00:06 +0100

    fixed test failures
    Co-Authored-By: Judith <39854388+felix-20@users.noreply.github.com>

--- src/sim_market.py
+++ src/sim_market.py
@@ -92,7 +92,7 @@ class LinearEconomy(SimMarket):
     def setup_act_obs_space(self):
         # cell 0: agent's quality, afterwards: odd cells: competitor's price, even cells: competitor's quality
         self.observation_space = gym.spaces.Box(
-            np.array([0.0] * (len(self.competitors) * 2.0 + 1.0)),
+            np.array([0.0] * (len(self.competitors) * 2 + 1)),
             np.array(
                 [ut.MAX_QUALITY]
                 + [ut.MAX_PRICE, ut.MAX_QUALITY] * len(self.competitors)
