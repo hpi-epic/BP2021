22165e5b1d3fecd0b78116aa5ace5a4e20e0c9bf
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-12-06 18:48:32 +0100

    Added possibility to monitor multiple agents
    Co-authored-by: Judith <felix-20@users.noreply.github.com>

--- src/agent_monitoring.py
+++ src/agent_monitoring.py
@@ -1,6 +1,5 @@
 import os
 import time
-from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,12 +11,13 @@ import sim_market as sim
 class Monitor():
 
 	def __init__(self) -> None:
+		self.enable_live_draws = True
 		self.episodes = 500
 		self.histogram_plot_interval = int(self.episodes / 10)
 		self.path_to_modelfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + os.sep + 'monitoring' + os.sep + 'test_marketplace.dat'
 		self.situation = 'linear'
 		self.marketplace = sim.CircularEconomy() if self.situation == 'circular' else sim.ClassicScenario()
-		self.agent = agent.QLearningAgent(self.marketplace.observation_space.shape[0], self.marketplace.action_space.n, load_path=self.path_to_modelfile)
+		self.agents = [agent.QLearningAgent(self.marketplace.observation_space.shape[0], self.marketplace.action_space.n, load_path=self.path_to_modelfile)]
 
 	# helper functions
 	def round_up(self, number, decimals=0):
@@ -25,8 +25,10 @@ class Monitor():
 		return np.ceil(number * multiplier) / multiplier
 
 	# configure the situation to be monitored
-	def setup_monitoring(self, new_episodes=None, new_interval=None, new_modelfile=None, new_situation=None, new_marketplace=None, new_agent=None) -> None:
+	def setup_monitoring(self, draw_enabled=None, new_episodes=None, new_interval=None, new_modelfile=None, new_situation=None, new_marketplace=None, new_agents=None) -> None:
 		# doesn't look nice, but afaik only way to keep parameter list short
+		if(draw_enabled is not None):
+			self.enable_live_draws = draw_enabled
 		if(new_episodes is not None):
 			self.episodes = new_episodes
 		if(new_interval is not None):
@@ -37,8 +39,8 @@ class Monitor():
 			self.situation = new_situation
 		if(new_marketplace is not None):
 			self.marketplace = new_marketplace
-		if(new_agent is not None):
-			self.agent = new_agent
+		if(new_agents is not None):
+			self.agents = new_agents
 
 	# metrics
 	def metrics_average(self, rewards) -> np.float64:
@@ -57,35 +59,37 @@ class Monitor():
 	def create_histogram(self, rewards) -> None:
 		plt.xlabel('Reward', fontsize='18')
 		plt.ylabel('Episodes', fontsize='18')
-		plt.hist(rewards, bins=10, color='#6e9bd1', align='mid', edgecolor='black', range=(0, self.round_up(int(self.metrics_maximum(rewards)), -3)))
-		plt.draw()
-		plt.pause(0.001)
-
-	# run simulation
-	def reset_episode(self, reward_per_episode, is_done, marketplace) -> Tuple[np.float64, bool, np.array]:
-		reward_per_episode = 0
-		is_done = False
-		state = marketplace.reset()
-
-		return reward_per_episode, is_done, state
+		plt.hist(rewards, bins=10, align='mid', edgecolor='black', range=(0, self.round_up(int(self.metrics_maximum(rewards)), -3)))
+		if self.enable_live_draws:
+			plt.draw()
+			plt.pause(0.001)
 
 	def run_marketplace(self) -> list:
-		# initialize marketplace
-		reward_per_episode = 0
+		# initialize the rewards list with a list for each agent
 		rewards = []
-		is_done = False
+		for i in range(len(self.agents)):
+			rewards.append([])
 
 		for episode in range(1, self.episodes + 1):
+			# reset the state once to be used by all agents
+			default_state = self.marketplace.reset()
 
-			reward_per_episode, is_done, state = self.reset_episode(reward_per_episode, is_done, self.marketplace)
+			for i in range(0, len(self.agents)):
+				# reset values for all agents
+				state = default_state
+				reward_per_episode = 0
+				is_done = False
 
-			while not is_done:
-				action = self.agent.policy(state)
-				state, reward, is_done, _ = self.marketplace.step(action)
-				reward_per_episode += reward
+				# run marketplace for this agent
+				while not is_done:
+					action = self.agents[i].policy(state)
+					state, reward, is_done, _ = self.marketplace.step(action)
+					reward_per_episode += reward
 
-			rewards.append(reward_per_episode)
+				# add the reward to the current agent's reward-Array
+				rewards[i].append(reward_per_episode)
 
+			# after all agents have run the episode
 			if (episode % 100) == 0:
 				print(f'Running {episode}th episode...')
 
@@ -99,20 +103,25 @@ monitor = Monitor()
 
 
 def main():
-	# TODO: config file for setup_monitoring?
-	# monitor.setup_monitoring(new_situation='circular')
+	# import agent
+	# monitor.setup_monitoring(new_agents=[monitor.agents[0], agent.FixedPriceAgent(6)])
 	print(f'Running', monitor.episodes, 'episodes')
 	print(f'Plot interval is:', monitor.histogram_plot_interval)
 	print(f'Using modelfile: ' + monitor.path_to_modelfile)
 	print(f'The situation is: ' + monitor.situation)
 	print(f'The marketplace is:', monitor.marketplace)
-	print(f'The agent is:', monitor.agent)
+	print(f'Monitoring these agents:')
+	for current_agent in monitor.agents:
+		print(current_agent)
+
 	rewards = monitor.run_marketplace()
 
-	print(f'The average reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_average(rewards)))
-	print(f'The median reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_median(rewards)))
-	print(f'The maximum reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_maximum(rewards)))
-	print(f'The minimum reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_minimum(rewards)))
+	for i in range(len(rewards)):
+		print(f'Statistics for agent:', monitor.agents[i])
+		print(f'The average reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_average(rewards[i])))
+		print(f'The median reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_median(rewards[i])))
+		print(f'The maximum reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_maximum(rewards[i])))
+		print(f'The minimum reward over {monitor.episodes} episodes is: ' + str(monitor.metrics_minimum(rewards[i])))
 
 	# save last histogram
 	fname = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + os.sep + 'monitoring' + os.sep + 'final_plot_' + time.strftime('%Y%m%d-%H%M%S') + '.svg'
--- src/tests/test_agent_monitoring.py
+++ src/tests/test_agent_monitoring.py
@@ -1,6 +1,8 @@
 import os
 import re
 
+import pytest
+
 from .context import Monitor
 from .context import agent_monitoring as am
 
@@ -12,6 +14,7 @@ def setup_function(function):
 	print('***SETUP***')
 	global monitor
 	monitor = Monitor()
+	monitor.setup_monitoring(draw_enabled=False)
 
 
 # create mock rewards list
@@ -25,13 +28,14 @@ def create_mock_rewards() -> list:
 # new_episodes, new_interval, new_modelfile, new_situation, new_marketplace, new_agent
 # values and types are mismatched on purpose, as we just want to make sure the global values are changed correctly, we don't work with them
 def test_setup_monitoring():
-	monitor.setup_monitoring(1, 2, 3, 4, 5, 6)
+	monitor.setup_monitoring(0, 1, 2, 3, 4, 5, 6)
+	assert 0 == monitor.enable_live_draws
 	assert 1 == monitor.episodes
 	assert 2 == monitor.histogram_plot_interval
 	assert 3 == monitor.path_to_modelfile
 	assert 4 == monitor.situation
 	assert 5 == monitor.marketplace
-	assert 6 == monitor.agent
+	assert 6 == monitor.agents
 
 
 def test_metrics_average():
@@ -54,30 +58,25 @@ def test_round_up():
 	assert monitor.round_up(999, -3) == 1000
 
 
-def test_create_histogram():
-	rewards = [100, 1000, 0, 1538]
+# all arrays in rewards must be of the same size
+def test_rewards_array_size():
+	rewards_wrong = [[1, 2], [1, 2, 3]]
+	with pytest.raises(Exception):
+		monitor.create_histogram(rewards_wrong)
+
+
+@pytest.mark.parametrize('rewards', ([100, 0], [[100, 0], [10, 5]], [[100, 0], [10, 5], [100, 10000], [10, 1000]]))
+def test_create_histogram(rewards):
 	monitor.create_histogram(rewards)
-
-
-def test_reset_episode():
-	reward_per_episode = 20
-	is_done = True
-	monitor.marketplace.state = []
-
-	reward_per_episode, is_done, monitor.marketplace.state = monitor.reset_episode(reward_per_episode, is_done, monitor.marketplace)
-
-	assert reward_per_episode == 0
-	assert is_done is False
-	assert len(monitor.marketplace.state) != 0
 
 
 def test_run_marketplace():
 	monitor.setup_monitoring(new_episodes=100, new_interval=100)
-	assert len(monitor.run_marketplace()) == 100
+	assert len(monitor.run_marketplace()[0]) == 100
 
 
 def test_main():
-	am.monitor.setup_monitoring(new_episodes=10, new_interval=10)
+	am.monitor.setup_monitoring(draw_enabled=False, new_episodes=10, new_interval=10)
 	am.main()
 	# make sure a final_plot was saved, and remove it after the test
 	for f in os.listdir('./monitoring'):
