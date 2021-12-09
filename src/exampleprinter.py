bc3ae6b456e92f79329b621f52726c99351c98a0
Blackjack <johann.schulze.tast@t-online.de>
2021-12-09 13:59:41 +0100

    Update exampleprinter.py

--- src/exampleprinter.py
+++ src/exampleprinter.py
@@ -7,18 +7,17 @@ import agent as a
 import sim_market
 
 
-def print_example(env=sim_market.CircularEconomyRebuyPrice(), agent=a.RuleBasedCERebuyAgent()):
+def print_example(env=sim_market.CircularEconomy(), agent=a.RuleBasedCEAgent()):
 	counter = 0
 	our_profit = 0
 	is_done = False
 	state = env.reset()
 	# print('The production price is', ut.PRODUCTION_PRICE)
-	print('hello')
 	writer = SummaryWriter()
 
 	with torch.no_grad():
 		while not is_done:
-			print(state)
+			# print(state)
 			action = agent.policy(state)
 			if isinstance(env, sim_market.CircularEconomy):
 				writer.add_scalar('Example_state/storage_content', env.state[0], counter)
@@ -32,22 +31,22 @@ def print_example(env=sim_market.CircularEconomyRebuyPrice(), agent=a.RuleBasedC
 				writer.add_scalar('Example_state/competitor_quality', env.state[2], counter)
 				writer.add_scalar('Example_state/competitor_price', env.state[1], counter)
 				writer.add_scalar('Example_action/price_agent', action + 1, counter)
-			print(
-				'This is the state:',
-				env.state,
-				' and I will do ',
-				action
-			)
+			# print(
+			# 	'This is the state:',
+			# 	env.state,
+			# 	' and I will do ',
+			# 	action
+			# )
 			state, reward, is_done, dict = env.step(action)
-			print('The agents profit this round is', reward)
+			# print('The agents profit this round is', reward)
 			our_profit += reward
 			writer.add_scalar('Example_reward/reward', reward, counter)
 			writer.add_scalar('Example_reward/reward_cumulated', our_profit, counter)
 			counter += 1
-	print(
-		'In total the agent earned',
-		our_profit
-	)
+	# print(
+	# 	'In total the agent earned',
+	# 	our_profit
+	# )
 	return our_profit
 
 
