ddfaa1aa33e3076dafbd5c029fb1cd2cb3719ae0
Johann Schulze Tast <35633229+blackjack2693@users.noreply.github.com>
2021-12-13 17:04:10 +0100

    Hotfix owner #91 (#106)
    * owner in its own class and abstract
    
    the owner was seperated and sim_market was made abstact
    
    * fixed some errors
    
    now it is also working
    
    * Update exampleprinter.py
    
    * fixed bug in sim_market
    
    The owner logic was not changed correctly in the last commit. this got fixed and caused the CI fail of last commit
    
    * added some comments to owner.py
    
    also made some changes to the method header
    
    * addressed some abstract conventions
    
    * addressed the comments by @jannikgro

--- src/exampleprinter.py
+++ src/exampleprinter.py
@@ -7,19 +7,18 @@ import agent as a
 import sim_market
 
 
-def print_example(env=sim_market.CircularEconomy(), agent=a.HumanPlayerCE()):
+def print_example(env=sim_market.CircularEconomy(), agent=a.RuleBasedCEAgent()):
 	counter = 0
 	our_profit = 0
 	is_done = False
 	state = env.reset()
 	# print('The production price is', ut.PRODUCTION_PRICE)
-
 	writer = SummaryWriter()
 
 	with torch.no_grad():
 		while not is_done:
-			print(state)
 			action = agent.policy(state)
+			print(state)
 			if isinstance(env, sim_market.CircularEconomy):
 				writer.add_scalar('Example_state/storage_content', env.state[0], counter)
 				writer.add_scalar('Example_state/products_in_circle', env.state[1], counter)
@@ -32,24 +31,34 @@ def print_example(env=sim_market.CircularEconomy(), agent=a.HumanPlayerCE()):
 				writer.add_scalar('Example_state/competitor_quality', env.state[2], counter)
 				writer.add_scalar('Example_state/competitor_price', env.state[1], counter)
 				writer.add_scalar('Example_action/price_agent', action + 1, counter)
-			print(
-				'This is the state:',
-				env.state,
-				' and I will do ',
-				action
-			)
+			# print(
+			# 	'This is the state:',
+			# 	env.state,
+			# 	' and I will do ',
+			# 	action
+			# )
 			state, reward, is_done, dict = env.step(action)
-			print('The agents profit this round is', reward)
+			# print('The agents profit this round is', reward)
 			our_profit += reward
 			writer.add_scalar('Example_reward/reward', reward, counter)
 			writer.add_scalar('Example_reward/reward_cumulated', our_profit, counter)
 			counter += 1
-	print(
-		'In total the agent earned',
-		our_profit
-	)
+	# print(
+	# 	'In total the agent earned',
+	# 	our_profit
+	# )
 	return our_profit
 
 
+def main():
+	agent = a.FixedPriceCERebuyAgent()
+	environment = sim_market.CircularEconomyRebuyPrice()
+	cumulative_reward = 0
+	for i in range(1000):
+		cumulative_reward += print_example(environment, agent)
+
+	print('The average reward is', cumulative_reward / 1000)
+
+
 if __name__ == '__main__':
-	print_example()
+	main()
