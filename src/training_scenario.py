4b2de4dd8cff11fd7986103a68e943b6ac47cb4e
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-12-14 10:29:48 +0100

    Renamed saved models to have agent and market names

--- src/training_scenario.py
+++ src/training_scenario.py
@@ -4,10 +4,10 @@ import agent
 import sim_market as sim
 import training
 
-economy = sim.CircularEconomy()
+economy = sim.CircularEconomyRebuyPrice()
 n_actions = 1
 for id in range(0, len(economy.action_space)):
 	n_actions *= economy.action_space[id].n
 
-RL_agent = agent.QLearningCEAgent(economy.observation_space.shape[0], n_actions, optim=torch.optim.Adam)
+RL_agent = agent.QLearningCERebuyAgent(economy.observation_space.shape[0], n_actions, optim=torch.optim.Adam)
 training.train_QLearning_agent(RL_agent, economy)
