8da039bdba6f9e9fadafe16a51d7288355096b54
NikkelM <nikkel.mollenhauer@student.hpi.de>
2021-12-15 10:52:13 +0100

    One-liner for device selection
    Co-authored-by: Judith <felix-20@users.noreply.github.com>

--- src/training_scenario.py
+++ src/training_scenario.py
@@ -10,10 +10,7 @@ n_actions = 1
 for id in range(0, len(economy.action_space)):
 	n_actions *= economy.action_space[id].n
 
-if torch.cuda.is_available():
-	device = 'cuda'
-else:
-	device = 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 RL_agent = agent.QLearningCERebuyAgent(n_observation=economy.observation_space.shape[0], n_actions=n_actions, device=device, optim=torch.optim.Adam)
 training.train_QLearning_agent(RL_agent, economy)
