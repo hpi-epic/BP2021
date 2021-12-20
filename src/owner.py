bf21ef61f45085c32ebe4641a5f84619cc364012
Jan Groeneveld <jan.groeneveld@student.hpi.de>
2021-12-21 00:18:01 +0100

    added docstring in customer and owner according to #125

--- src/owner.py
+++ src/owner.py
@@ -10,46 +10,53 @@ import utils as ut
 
 class Owner(ABC):
 	"""
-	The abstract class represents the owner of a product and is responsible for the decision what to do with the product.
+	The abstract class represents the owner of a product and is responsible for the decision of whether to return the product or not.
 	"""
 
 	@abstractmethod
-	def consider_return(self, others):
+	def generate_return_probabilities_from_offer(self, offers) -> np.array:
+		"""This abstract method receives offers and generates the probabilities for the possible owner actions. An owner can throw away his product, he can hold his product or return it to one of the vendors
+
+		Args:
+			offers (np.array): a rebuy offer an owner receives
+
+		Returns:
+			np.array: The first entry is the probability that the owner throws away his product. The second entry is the probability that the owner holds his product. Afterwards, for all vendors the probabilities of returning the product to him follows.
+		"""
 		raise NotImplementedError
 
 
-class OwnerReturn(Owner):
-	"""
-	The class represents the owner of a product and is responsible for the decision of whether to return the product or not.
-	"""
-
-	def consider_return(self) -> None:
-		"""
-		The function returns the rebuy decision based on the probabilities calculated in the function set_probabilities_from_offer.
-		The set_probabilities_from_offer function is not needed for this owner to function properly.
-		"""
-		return int(np.floor(np.random.rand() * 2))
-
-
-class OwnerRebuy(OwnerReturn):
-	"""
-		In contrast to its superclass, this owner sells the product rather than returning it out of good will.
-		For this it calculates a probability distribution of what to do with the product with every new offer and smaples from it.
-	"""
-	def __init__(self) -> None:
-		super().__init__()
-		self.probabilities = None
-
-	def set_probabilities_from_offer(self, offer) -> None:
-		"""
-		The function calculates the probability distribution of a rebuy depending on the offer given by the buyer.
+class UniformDistributionOwner(Owner):
+	def generate_return_probabilities_from_offer(self, offers) -> np.array:
+		"""This method generates a uniform distribution over all vendors to return the product without considering the rebuy price and the overall market situation.
+		It assumes three entries per vendor (refurbished price, new price and in_storage but NO rebuy price)
 
 		Args:
-			offer (np.array(int)): The offer given by the vendor.
+			offers (np.array): The content is ignored in this method. It is just used to determine the number of options which is the number of vendors plus 2 (throw away, hold the product).
+
+		Returns:
+			np.array: a uniform distribution over all possible actions
 		"""
-		price_refurbished = offer[2] + 1
-		price_new = offer[3] + 1
-		price_rebuy = offer[4] + 1
+		number_of_options = np.floor(len(offers) / 3) + 2
+		return np.array([1 / number_of_options] * number_of_options)
+
+
+class OwnerRebuy(Owner):
+	def generate_return_probabilities_from_offer(self, offers) -> np.array:
+		"""This method tries a more sophisticated version of generating return probabilities.
+		The owner likes if the rebuy price is close to the price in the sell offer.
+		That will increase the probability that he will return his product.
+		If the rebuy price is very low, the owner will just throw away his product more often. Holding the product is the fallback option.
+
+		Args:
+			offers (np.array): The method assumes one entry for the number of products in circulation and four entries for each vendor (refurbished price, new price, rebuy price and in_storage)
+
+		Returns:
+			np.array: probability distribution over all possible actions.
+		"""
+		price_refurbished = offers[2] + 1
+		price_new = offers[3] + 1
+		price_rebuy = offers[4] + 1
 
 		holding_preference = 1
 
@@ -59,11 +66,4 @@ class OwnerRebuy(OwnerReturn):
 		# Customer is very excited if the value of his product is close to the new or refurbished price
 		return_preference = 2 * np.exp((price_rebuy - min(price_refurbished, price_new)) / min(price_refurbished, price_new))
 
-		self.probabilities = ut.softmax(np.array([holding_preference, discard_preference, return_preference]))
-
-	def consider_return(self) -> None:
-		"""
-		The function returns the rebuy decision based on the probabilities calculated in the function set_probabilities_from_offer. It has to be called first.
-		"""
-		assert self.probabilities is not None, 'Probabilities not set. You need to call set_probabilities_from_offer first.'
-		return ut.shuffle_from_probabilities(self.probabilities)
+		return ut.softmax(np.array([holding_preference, discard_preference, return_preference]))
