565aa2c3386bf935bdf5e165d8f3725cd18f3943
Jan Groeneveld <jan.groeneveld@student.hpi.de>
2022-01-02 18:52:26 +0100

    experimental ac implementation

--- src/training_scenario.py
+++ src/training_scenario.py
@@ -4,10 +4,10 @@ import sim_market as sim
 import training
 import vendors
 
-economy = sim.CircularEconomyRebuyPriceOneCompetitor()
+economy = sim.ClassicScenario()
 n_actions = 1
-for id in range(0, len(economy.action_space)):
-	n_actions *= economy.action_space[id].n
+# for id in range(0, len(economy.action_space)):
+# 	n_actions *= economy.action_space[id].n
 
-RL_agent = vendors.QLearningCERebuyAgent(n_observation=economy.observation_space.shape[0], n_actions=n_actions, optim=torch.optim.Adam)
+RL_agent = vendors.QLearningAgent(n_observation=economy.observation_space.shape[0], n_actions=10, optim=torch.optim.Adam)
 training.train_QLearning_agent(RL_agent, economy)
