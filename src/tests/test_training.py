b03473748cd414948eb3b68070eb670ea7ecf277
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-01-09 14:50:37 +0100

    Fixed test_training by passing classes not instances

--- src/tests/test_training.py
+++ src/tests/test_training.py
@@ -1,42 +1,43 @@
-# import os
-# import re
-# import shutil
-# from importlib import reload
-# from unittest.mock import mock_open, patch
+import os
+import re
+import shutil
+from importlib import reload
+from unittest.mock import mock_open, patch
 
-# import pytest
-# import torch
+import pytest
+import torch
 
-# import agents.vendors as vendors
-# import configuration.config as config
-# import market.sim_market as sim_market
-# import rl.training as training
-# import tests.utils_tests as ut_t
-
-
-# # teardown after each test
-# def teardown_module(module):
-# 	print('***TEARDOWN***')
-# 	for f in os.listdir('./runs'):
-# 		if re.match('test_*', f):
-# 			shutil.rmtree('./runs/' + f)
+import agents.vendors as vendors
+import configuration.config as config
+import market.sim_market as sim_market
+import rl.training as training
+import tests.utils_tests as ut_t
 
 
-# test_scenarios = [
-# 	(sim_market.ClassicScenario(), vendors.QLearningAgent(sim_market.ClassicScenario().observation_space.shape[0], n_actions=10, optim=torch.optim.Adam)),
-# 	(sim_market.MultiCompetitorScenario(), vendors.QLearningAgent(sim_market.MultiCompetitorScenario().observation_space.shape[0], n_actions=10, optim=torch.optim.Adam)),
-# 	(sim_market.CircularEconomyMonopolyScenario(), vendors.QLearningCEAgent(sim_market.CircularEconomyMonopolyScenario().observation_space.shape[0], n_actions=100, optim=torch.optim.Adam)),
-# 	(sim_market.CircularEconomyRebuyPriceMonopolyScenario(), vendors.QLearningCERebuyAgent(sim_market.CircularEconomyRebuyPriceMonopolyScenario().observation_space.shape[0], n_actions=100, optim=torch.optim.Adam)),
-# 	(sim_market.CircularEconomyRebuyPriceOneCompetitor(), vendors.QLearningCERebuyAgent(sim_market.CircularEconomyRebuyPriceOneCompetitor().observation_space.shape[0], n_actions=100, optim=torch.optim.Adam))
-# ]
+# teardown after each test
+def teardown_module(module):
+	print('***TEARDOWN***')
+	for f in os.listdir('./runs'):
+		if re.match('test_*', f):
+			shutil.rmtree('./runs/' + f)
 
 
-# @pytest.mark.parametrize('environment, agent', test_scenarios)
-# def test_market_scenario(environment, agent):
-# 	json = ut_t.create_mock_json(rl=ut_t.create_mock_json_rl(replay_start_size='500', sync_target_frames='100'))
-# 	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, json)
-# 		# Include utils_rl again to make sure the file is read again
-# 		reload(config)
-# 		training.train_QLearning_agent(agent, environment, int(config.REPLAY_START_SIZE * 1.2), log_dir_prepend='test_')
-# 	reload(config)
+test_scenarios = [
+	(sim_market.ClassicScenario, vendors.QLearningAgent, 10),
+	(sim_market.MultiCompetitorScenario, vendors.QLearningAgent, 10),
+	(sim_market.CircularEconomyMonopolyScenario, vendors.QLearningCEAgent, 100),
+	(sim_market.CircularEconomyRebuyPriceMonopolyScenario, vendors.QLearningCERebuyAgent, 100),
+	(sim_market.CircularEconomyRebuyPriceOneCompetitor, vendors.QLearningCERebuyAgent, 100)
+]
+
+
+@pytest.mark.parametrize('environment, agent, n_actions', test_scenarios)
+def test_market_scenario(environment, agent, n_actions):
+	json = ut_t.create_mock_json(rl=ut_t.create_mock_json_rl(replay_start_size='500', sync_target_frames='100'))
+	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
+		ut_t.check_mock_file(mock_file, json)
+		# Include utils_rl again to make sure the file is read again
+		reload(config)
+		environment = environment()
+		agent = agent(environment.observation_space.shape[0], n_actions=n_actions, optim=torch.optim.Adam)
+		training.train_QLearning_agent(agent, environment, int(config.REPLAY_START_SIZE * 1.2), log_dir_prepend='test_')
