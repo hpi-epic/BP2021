68453d13b4a708b7b983a7181b31db4e2cafe118
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2022-01-11 14:22:05 +0100

    Introduce the `MarketOverview.svg` (#160)
    * Created `AlphaBusiness`-package
    
    * Added README-entry for package installation
    
    * Update env
    
    * Install only AlphaBusiness
    
    * Comment out test
    
    * Update pip version
    
    * Use time.perf_counter() for more accuracy
    
    * Update README.md
    
    * Fix conflicts
    
    * Introduce new folder structure #126
    
    * Add pre-commit badge :)
    
    * Removed __init__.py files
    
    * Re-Add `__init__.py` files for source discovery/coverage reporting in pytest
    
    * Better excludes for test coverage
    
    * Reformat the svg
    
    * Minify svg
    
    * Move `MarketOverview.svg` to `monitoring` folder
    
    * First set of formatting changes
    
    * More reformatting
    
    * Removed unnecessary code
    
    * Added placeholders where variables should be substituted
    
    * First demo of the svg manipulation
    
    * First version of dictionary containing the placeholders
    
    * Finalized template dictionary and .svg file
    
    * Change default value to be the empty string
    
    * Add tests
    
    * refactoring of svg_manipulation, started working on #158
    
    * sorted svg placeholders alphabetically, changed saving of svg
    
    * save overview svg in the end of exampleprinter
    
    * Fix main()
    
    * adjusted template svg
    
    Co-Authored-By: jannikgro <52510222+jannikgro@users.noreply.github.com>
    
    * modified template, removed Purchases
    
    Co-Authored-By: jannikgro <52510222+jannikgro@users.noreply.github.com>
    
    * added values to svg, saved every episode and converted to gif in the end
    
    Co-Authored-By: jannikgro <52510222+jannikgro@users.noreply.github.com>
    
    * added svglib to environment
    
    Co-Authored-By: jannikgro <52510222+jannikgro@users.noreply.github.com>
    
    * inserted throw_away at arrow and profit
    
    * added new keys
    
    * added new dicts and introduced cumulated dict
    
    * fixed failing exampleprinter tests
    
    * changed my_gif to examplerun
    
    * adjusted svg layout
    
    * not instantiate SVGManipulator if the ecomomy is not CircularEconomyRebuyPriceOneCompetitor
    
    * not instantiate SVGManipulator if the ecomomy is not CircularEconomyRebuyPriceOneCompetitor
    
    * removed logdir from summarywriter
    
    * removed underscore from MarketOverview_template.svg
    
    * activated logdir again
    
    * scaled up gif
    
    * svgs can be viewed on an html page
    
    * added docstrings to svg_manipulation
    
    * new indentation for html
    
    * first test 100 % mocked for svg
    
    * changed paths in svg manipulation
    
    * changed svgs to be as wide as your screen
    
    * already existing test work with mocking now
    
    * introduce an expected output svg for testing purpose
    
    * more tests for svg manipulation
    
    * use `os.pardir`, added assert statements
    
    * added more tests for svg_manipulation
    
    * Some refactors while reviewing
    
    * Fix tests
    
    * Remove duplicated string
    
    * inserted simulation name, chaged episodes to time step
    
    * removed gif functionality
    
    * removed global for read only in tests
    
    * Removed log_dir from SummaryWriter
    
    * Better comment
    
    * changed MarketOverview box for customer
    
    * implemented #188 for svg_manipulation
    
    * changed write_to in utils
    
    * renamed svg_template to template_svg
    
    * fixed the test to template_svg
    
    * fixed broken code after merge of dev
    
    Co-authored-by: Judith <39854388+felix-20@users.noreply.github.com>
    Co-authored-by: jannikgro <52510222+jannikgro@users.noreply.github.com>
    Co-authored-by: Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
    Co-authored-by: Blackjack <johann.schulze.tast@t-online.de>

--- src/configuration/utils.py
+++ src/configuration/utils.py
@@ -27,7 +27,8 @@ def shuffle_from_probabilities(probabilities) -> int:
 
 
 def write_dict_to_tensorboard(writer, dictionary, counter, is_cumulative=False) -> None:
-	"""This function takes a dictionary of data with data from one step and adds it at the specified time to the tensorboard.
+	"""
+	This function takes a dictionary of data with data from one step and adds it at the specified time to the tensorboard.
 
 	Args:
 		writer (SummaryWriter): The tensorboard writer on which the calls to write are taken.
@@ -50,7 +51,8 @@ def write_dict_to_tensorboard(writer, dictionary, counter, is_cumulative=False) 
 
 
 def divide_content_of_dict(dict1, divisor) -> dict:
-	"""Recursively divide a dictionary which contains only numbers by a divisor
+	"""
+	Recursively divide a dictionary which contains only numbers by a divisor
 
 	Args:
 		dict1 (dict): the dictionary you would like to divide
@@ -70,7 +72,8 @@ def divide_content_of_dict(dict1, divisor) -> dict:
 
 
 def add_content_of_two_dicts(dict1, dict2) -> dict:
-	"""This function takes two dicts and runs recursively through the dicts. The dicts must have the same structure.
+	"""
+	This function takes two dicts and runs recursively through the dicts. The dicts must have the same structure.
 
 	Args:
 		dict1 (dict): first dictionary you want to add
@@ -89,3 +92,46 @@ def add_content_of_two_dicts(dict1, dict2) -> dict:
 			assert isinstance(dict2[key], int) or isinstance(dict2[key], float), 'dict2 should only contain numbers (int or float)'
 			newdict[key] = dict1[key] + dict2[key]
 	return newdict
+
+
+def write_content_of_dict_to_overview_svg(manipulator, episode, episode_dictionary, cumulated_dictionary) -> None:
+	"""
+	This function takes a SVGManipulator and two dictionaries and translates the svg placeholder to the values in the dictionary
+
+	Args:
+		manipulator (SVGManipulator): instance of the class that can modify the Marketoverview_template.svg
+		episode (int): current time step
+		episode_dictionary (dict): monitoring dictionary of the current time step
+		cumulated_dictionary (dict): monitoring dictionary with accumulated values for episodes
+	"""
+	episode += 1
+	translated_dict = {}
+	translated_dict['simulation_name'] = 'Market Simulation'
+	translated_dict['simulation_episode_length'] = str(config.EPISODE_LENGTH)
+	translated_dict['simulation_current_episode'] = str(episode)
+	translated_dict['consumer_total_arrivals'] = str(episode * config.NUMBER_OF_CUSTOMERS)
+	translated_dict['consumer_total_sales'] = str(episode * config.NUMBER_OF_CUSTOMERS - cumulated_dictionary['customer/buy_nothing'])
+	translated_dict['a_competitor_name'] = 'vendor_0'
+	translated_dict['a_throw_away'] = str(episode_dictionary['owner/throw_away'])
+	translated_dict['a_garbage'] = str(cumulated_dictionary['owner/throw_away'])
+	translated_dict['a_inventory'] = str(episode_dictionary['state/in_storage']['vendor_0'])
+	translated_dict['a_profit'] = str(cumulated_dictionary['profits/all']['vendor_0'])
+	translated_dict['a_price_new'] = str(episode_dictionary['actions/price_new']['vendor_0'] + 1)
+	translated_dict['a_price_used'] = str(episode_dictionary['actions/price_refurbished']['vendor_0'] + 1)
+	translated_dict['a_rebuy_price'] = str(episode_dictionary['actions/price_rebuy']['vendor_0'] + 1)
+	translated_dict['a_repurchases'] = str(episode_dictionary['owner/rebuys']['vendor_0'])
+	translated_dict['a_resource_cost'] = str(config.PRODUCTION_PRICE)
+	translated_dict['a_resources_in_use'] = str(episode_dictionary['state/in_circulation'])
+	translated_dict['a_sales_new'] = str(episode_dictionary['customer/purchases_new']['vendor_0'])
+	translated_dict['a_sales_used'] = str(episode_dictionary['customer/purchases_refurbished']['vendor_0'])
+	translated_dict['b_competitor_name'] = 'vendor_1'
+	translated_dict['b_inventory'] = str(episode_dictionary['state/in_storage']['vendor_1'])
+	translated_dict['b_profit'] = str(cumulated_dictionary['profits/all']['vendor_1'])
+	translated_dict['b_price_new'] = str(episode_dictionary['actions/price_new']['vendor_1'] + 1)
+	translated_dict['b_price_used'] = str(episode_dictionary['actions/price_refurbished']['vendor_1'] + 1)
+	translated_dict['b_rebuy_price'] = str(episode_dictionary['actions/price_rebuy']['vendor_1'] + 1)
+	translated_dict['b_repurchases'] = str(episode_dictionary['owner/rebuys']['vendor_1'])
+	translated_dict['b_resource_cost'] = str(config.PRODUCTION_PRICE)
+	translated_dict['b_sales_new'] = str(episode_dictionary['customer/purchases_new']['vendor_1'])
+	translated_dict['b_sales_used'] = str(episode_dictionary['customer/purchases_refurbished']['vendor_1'])
+	manipulator.write_dict_to_svg(target_dictionary=translated_dict)
