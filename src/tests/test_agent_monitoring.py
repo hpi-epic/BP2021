4eb7c6eadfb2aef85f96aeb8edb7acb4f50e432a
Blackjack <johann.schulze.tast@t-online.de>
2022-01-17 16:37:11 +0100

    fixed error after merge

--- src/tests/test_agent_monitoring.py
+++ src/tests/test_agent_monitoring.py
@@ -130,7 +130,7 @@ def test_get_modelfile_path():
 	with patch('monitoring.agent_monitoring.os.path.exists') as mock_exists:
 		mock_exists.return_value = False
 		with pytest.raises(AssertionError) as assertion_message:
-			monitor.get_modelfile_path('non_existing_modelfile.dat')
+			monitor._get_modelfile_path('non_existing_modelfile.dat')
 		assert 'the specified modelfile does not exist' in str(assertion_message.value)
 
 
