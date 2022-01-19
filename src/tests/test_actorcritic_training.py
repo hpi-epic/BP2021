a7d40a88a2afd286054f894efd3c36ca39541805
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-01-19 18:30:30 +0100

    Mock SummaryWriter in tests

--- src/tests/test_actorcritic_training.py
+++ src/tests/test_actorcritic_training.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 import market.sim_market as sim_market
@@ -6,23 +8,25 @@ import rl.actorcritic_training as a2c_training
 
 
 def test_standard_setup():
-    a2c_training.train_actorcritic()
+	with patch('rl.actorcritic_training.SummaryWriter'):
+		a2c_training.train_actorcritic()
 
 
 test_scenarios = [
-    (sim_market.ClassicScenario, a2c_agent.DiscreteACALinear, True),
-    (sim_market.ClassicScenario, a2c_agent.ContinuosActorCriticAgent, True),
-    (sim_market.MultiCompetitorScenario, a2c_agent.DiscreteACALinear, False),
-    (sim_market.MultiCompetitorScenario, a2c_agent.ContinuosActorCriticAgent, False),
-    (sim_market.CircularEconomyMonopolyScenario, a2c_agent.DiscreteACACircularEconomy, True),
-    (sim_market.CircularEconomyMonopolyScenario, a2c_agent.ContinuosActorCriticAgent, False),
-    (sim_market.CircularEconomyRebuyPriceMonopolyScenario, a2c_agent.DiscreteACACircularEconomyRebuy, True),
-    (sim_market.CircularEconomyRebuyPriceMonopolyScenario, a2c_agent.ContinuosActorCriticAgent, False),
-    (sim_market.CircularEconomyRebuyPriceOneCompetitor, a2c_agent.DiscreteACACircularEconomyRebuy, False),
-    (sim_market.CircularEconomyRebuyPriceOneCompetitor, a2c_agent.ContinuosActorCriticAgent, True)
+	(sim_market.ClassicScenario, a2c_agent.DiscreteACALinear, True),
+	(sim_market.ClassicScenario, a2c_agent.ContinuosActorCriticAgent, True),
+	(sim_market.MultiCompetitorScenario, a2c_agent.DiscreteACALinear, False),
+	(sim_market.MultiCompetitorScenario, a2c_agent.ContinuosActorCriticAgent, False),
+	(sim_market.CircularEconomyMonopolyScenario, a2c_agent.DiscreteACACircularEconomy, True),
+	(sim_market.CircularEconomyMonopolyScenario, a2c_agent.ContinuosActorCriticAgent, False),
+	(sim_market.CircularEconomyRebuyPriceMonopolyScenario, a2c_agent.DiscreteACACircularEconomyRebuy, True),
+	(sim_market.CircularEconomyRebuyPriceMonopolyScenario, a2c_agent.ContinuosActorCriticAgent, False),
+	(sim_market.CircularEconomyRebuyPriceOneCompetitor, a2c_agent.DiscreteACACircularEconomyRebuy, False),
+	(sim_market.CircularEconomyRebuyPriceOneCompetitor, a2c_agent.ContinuosActorCriticAgent, True)
 ]
 
 
 @pytest.mark.parametrize('marketplace, agent, verbose', test_scenarios)
 def test_training_configurations(marketplace, agent, verbose):
-    a2c_training.train_actorcritic(marketplace_class=marketplace, agent_class=agent, verbose=verbose, number_of_training_steps=120, total_envs=64)
+	with patch('rl.actorcritic_training.SummaryWriter'):
+		a2c_training.train_actorcritic(marketplace_class=marketplace, agent_class=agent, verbose=verbose, number_of_training_steps=80, total_envs=48)
