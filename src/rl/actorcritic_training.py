2a3748e37b87ce07dc999d9b74628867ceee5832
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2022-01-20 23:34:50 +0100

    changed to config.SYNC_TARGET_FRAMES again

--- src/rl/actorcritic_training.py
+++ src/rl/actorcritic_training.py
@@ -105,7 +105,7 @@ def train_actorcritic(marketplace_class=sim_market.CircularEconomyRebuyPriceOneC
 		valueloss, policy_loss = agent.train_batch(torch.Tensor(np.array(states)), torch.from_numpy(np.array(actions, dtype=np.int64)), torch.Tensor(np.array(rewards)), torch.Tensor(np.array(next_state)), finished_episodes <= 500)
 		all_value_losses.append(valueloss)
 		all_policy_losses.append(policy_loss)
-		if (step_number + 1) % 200 == 0:
+		if (step_number + 1) % config.SYNC_TARGET_FRAMES == 0:
 			agent.synchronize_critic_tgt_net()
 
 
