74076aa7b6303f6ded61e3ceeea9dc9ef8315b91
Judith <39854388+felix-20@users.noreply.github.com>
2022-01-23 16:36:52 +0100

    updated gitignore to ignore the configuration folder in the webserver

--- webserver/alpha_business_app/handle_uploading.py
+++ webserver/alpha_business_app/handle_uploading.py
@@ -1,5 +1,10 @@
+import os
+
 def handle_uploaded_file(f):
-	print(f)
-	with open('./my_config.json', 'wb+') as destination:
+	path_to_configurations = './configurations'
+	if not os.path.exists(path_to_configurations):
+		os.mkdir(path_to_configurations)
+
+	with open(os.path.join(path_to_configurations, 'my_config.json'), 'wb+') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
