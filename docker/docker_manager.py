6d5c478f173651434c4aa4ab4ed72b891926757a
Blackjack <johann.schulze.tast@t-online.de>
2022-01-29 14:19:39 +0100

    fixed pre-commit

--- docker/docker_manager.py
+++ docker/docker_manager.py
@@ -1,6 +1,7 @@
+import json
 import os
 import time
-import json
+
 import docker
 
 
@@ -88,11 +89,11 @@ class DockerManager():
 		# create a device request to use all available GPU devices with compute capabilities
 		if use_gpu:
 			device_request_gpu = docker.types.DeviceRequest(driver='nvidia', count=-1, capabilities=[['compute']])
-			container = self._client.containers.create(image_id, 
-														name=f'{container_name}_container', 
-														detach=True, 
-														ports={'6006/tcp': 6006},
-														device_requests=[device_request_gpu])
+			container = self._client.containers.create(image_id,
+				name=f'{container_name}_container',
+				detach=True,
+				ports={'6006/tcp': 6006},
+				device_requests=[device_request_gpu])
 		else:
 			container = self._client.containers.create(image_id, name=f'{container_name}_container', detach=True, ports={'6006/tcp': 6006})
 
@@ -137,7 +138,6 @@ class DockerManager():
 			str: The status of the container
 		"""
 		return self._client.containers.get(container_id).status
-
 
 	def get_container_logs(self, container_id: str, timestamps: bool = False) -> str:
 		"""
@@ -226,7 +226,7 @@ class DockerManager():
 		# assert self.is_container_running(container_id), f'the Container is not running: {container_id}'
 		self.execute_command(container_id, 'mkdir ./results/runs/')
 		self.execute_command(container_id, 'tensorboard serve --logdir ./results/runs --bind_all')
-		return DockerInfo(container_id,data='http://localhost:6006')
+		return DockerInfo(container_id, data='http://localhost:6006')
 
 	def upload_file(self, container_id: str, src_path: str, dest_path: str) -> None:
 		"""
@@ -271,6 +271,7 @@ class DockerManager():
 
 	def health(self, container_id: str) -> DockerInfo:
 		return DockerInfo(container_id, status=self.container_status(container_id))
+
 
 if __name__ == '__main__':
 	manager = DockerManager()
