ca66d8113ca56990b6d2d69f8f9e090f116ed5b6
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-01-30 15:24:47 +0100

    Removed fake_docker_manager.py, activated docstring coverage with lower percentage for failure, added docstrings to app.py

--- docker/app.py
+++ docker/app.py
@@ -20,23 +20,48 @@ manager = DockerManager()
 app = FastAPI()
 
 
-# works
 @app.post('/start')
-async def start_container(config: Request):
+async def start_container(config: Request) -> JSONResponse:
+	"""
+	Start a container with the specified config.json.
+
+	Args:
+		config (Request): The config.json file that should be sent to the container.
+
+	Returns:
+		JSONResponse: The response of the Docker start request.
+	"""
 	container_info = manager.start(await config.json())
 	return JSONResponse(vars(container_info))
 
 
-# works
 @app.get('/health/')
-async def is_container_alive(id: str):
+async def is_container_alive(id: str) -> JSONResponse:
+	"""
+	Check if the container is still running.
+
+	Args:
+		id (str): The id of the container.
+
+	Returns:
+		JSONResponse: The response of the status request.
+	"""
 	container_info = manager.health(id)
 	return JSONResponse(vars(container_info))
 
 
-# works, downloads the tar archive
 @app.get('/data/')
-async def get_container_data(id: str, path: str = None):
+async def get_container_data(id: str, path: str = None) -> StreamingResponse:
+	"""
+	Extract a folder or file from a container.
+
+	Args:
+		id (str): The id of the container.
+		path (str): The path of the folder or file that should be extracted.
+
+	Returns:
+		StreamingResponse: A stream generator that will download the requested path as a .tar archive.
+	"""
 	if path:
 		container_info = manager.get_container_data(id, path)
 	else:
@@ -46,43 +71,78 @@ async def get_container_data(id: str, path: str = None):
 		media_type='application/x-tar')
 
 
-# works
 @app.get('/exec/')
-async def execute_command(id: str, command: str):
+async def execute_command(id: str, command: str) -> StreamingResponse:
+	"""
+	Execute a command in a container.
+
+	Args:
+		id (str): The id of the container.
+		command (str): The key of the command to execute, which will be mapped to the python command internally if it is valid.
+
+	Returns:
+		StreamingResponse: A stream generator that will return the stdout the container produces from the command.
+	"""
 	container_info = manager.execute_command(id, command)
 	return StreamingResponse(vars(container_info)['stream'])
 
 
-# works, returns a bool signaling whether it worked
 @app.post('/upload')
-async def upload_config(id: str, config: Request):
+async def upload_config(id: str, config: Request) -> JSONResponse:
+	"""
+	Upload a config.json to the container.
+
+	Args:
+		id (str): The id of the container.
+		config (Request): The config.json that should be uploaded.
+
+	Returns:
+		JSONResponse: The response of the upload request.
+	"""
 	container_info = manager.upload_config(id, await config.json())
 	return JSONResponse(vars(container_info))
 
 
-# works
 @app.get('/stop/')
-async def stop_container(id: str):
+async def stop_container(id: str) -> JSONResponse:
+	"""
+	Stop the container. Does not remove it.
+
+	Args:
+		id (str): The id of the container.
+
+	Returns:
+		JSONResponse: The response of the stop request.
+	"""
 	container_info = manager.stop_container(id)
 	return JSONResponse(vars(container_info))
 
 
-# works
 @app.get('/data/tensorboard/')
-async def get_tensorboard_link(id: str):
+async def get_tensorboard_link(id: str) -> RedirectResponse:
+	"""
+	Start a tensorboard session in the container.
+
+	Args:
+		id (str): The id of the container.
+
+	Returns:
+		RedirectResponse: A redirect to the tensorboard endpoint.
+	"""
 	tb_link = manager.start_tensorboard(id)
 	return RedirectResponse(vars(tb_link)['data'])
 
 
-# works, returns 'removed' and stops the container if it still runs
 @app.get('/remove/')
-async def remove_container(id: str):
-	container_info = manager.remove_container(id)
-	return JSONResponse(vars(container_info))
+async def remove_container(id: str) -> JSONResponse:
+	"""
+	Stop and remove the container.
 
+	Args:
+		id (str): The id of the container.
 
-@app.get('/kill/')
-async def kill_container(id: str):
-	# deprecated in favor of remove
+	Returns:
+		JSONResponse: The response of the remove request.
+	"""
 	container_info = manager.remove_container(id)
 	return JSONResponse(vars(container_info))
