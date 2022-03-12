19c2a50bca86ac99ca6f8c90ab783fc7123aea23
Jan Groeneveld <jan.groeneveld@student.hpi.de>
2022-03-12 22:34:02 +0100

    linter fixing

--- src/tests/test_customers.py
+++ src/tests/test_customers.py
@@ -64,35 +64,46 @@ def test_customer_action_range(customer, market):
 	assert len(probability_distribution) == market()._get_number_of_vendors() * \
 		(1 if issubclass(market, linear_market.LinearEconomy) else 2) + 1
 
+
 def test_linear_higher_price_lower_purchase_probability():
 	common_state, vendor_specific_state, vendor_actions = np.array([]), [[12], [12]], [3, 5]
-	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer(customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
+	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer( \
+		customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
 	assert probability_distribution[1] > probability_distribution[2]
+
 
 def test_linear_higher_quality_higher_purchase_probability():
 	common_state, vendor_specific_state, vendor_actions = np.array([]), [[13], [12]], [3, 3]
-	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer(customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
+	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer( \
+		customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
 	assert probability_distribution[1] > probability_distribution[2]
+
 
 def test_equal_ratio_equal_purchase_probability():
 	# In the following line: [3, 1] means prices [4, 2]
 	common_state, vendor_specific_state, vendor_actions = np.array([]), [[16], [8]], [3, 1]
-	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer(customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
+	probability_distribution = customer.CustomerLinear.generate_purchase_probabilities_from_offer( \
+		customer.CustomerLinear, common_state, vendor_specific_state, vendor_actions)
 	assert probability_distribution[1] == probability_distribution[2]
+
 
 def test_linear_lower_overall_price_lower_nothing_probability():
 	common_state1, vendor_specific_state1, vendor_actions1 = np.array([]), [[15], [15]], [3, 3]
-	probability_distribution1 = customer.CustomerLinear.generate_purchase_probabilities_from_offer(customer.CustomerLinear, common_state1, vendor_specific_state1, vendor_actions1)
+	probability_distribution1 = customer.CustomerLinear.generate_purchase_probabilities_from_offer( \
+		customer.CustomerLinear, common_state1, vendor_specific_state1, vendor_actions1)
 	common_state2, vendor_specific_state2, vendor_actions2 = np.array([]), [[15], [15]], [4, 4]
-	probability_distribution2 = customer.CustomerLinear.generate_purchase_probabilities_from_offer(customer.CustomerLinear, common_state2, vendor_specific_state2, vendor_actions2)
+	probability_distribution2 = customer.CustomerLinear.generate_purchase_probabilities_from_offer( \
+		customer.CustomerLinear, common_state2, vendor_specific_state2, vendor_actions2)
 	print(probability_distribution1)
 	print(probability_distribution2)
 	assert probability_distribution1[0] < probability_distribution2[0]
 	assert probability_distribution1[1] > probability_distribution2[1]
 	assert probability_distribution1[2] > probability_distribution2[2]
 
+
 def test_circular_higher_price_lower_purchase_probability():
 	common_state, vendor_specific_state, vendor_actions = np.array([]), [[17], [23]], [[3, 6], [4, 5]]
-	probability_distribution = customer.CustomerCircular.generate_purchase_probabilities_from_offer(customer.CustomerCircular, common_state, vendor_specific_state, vendor_actions)
+	probability_distribution = customer.CustomerCircular.generate_purchase_probabilities_from_offer( \
+		customer.CustomerCircular, common_state, vendor_specific_state, vendor_actions)
 	assert probability_distribution[1] > probability_distribution[3]
 	assert probability_distribution[2] < probability_distribution[4]
