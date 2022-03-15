a4c725d1f4632589e088254c86c16208b7932b58
Johann Schulze Tast <Johann.SchulzeTast@student.hpi.de>
2022-03-15 23:54:44 +0000

    replaced try catch with stackoverflow solution

--- webserver/alpha_business_app/views.py
+++ webserver/alpha_business_app/views.py
@@ -6,23 +6,17 @@ from .forms import UploadFileForm
 from .handle_files import handle_uploaded_file
 from .models import Container
 
-
 def detail(request, container_id) -> HttpResponse:
 	try:
 		wanted_container = Container.objects.get(id=container_id)
 	except Container.DoesNotExist as error:
 		raise Http404('Container does not exist') from error
-	# in case we just enter the site, we have no action to get from the request
-	try:
-		
-		if request.POST['action'] == 'remove':
-			# if we  want to remove the container, we need to redirect to a different site
-			button_handler = ButtonHandler(request, view='observe.html', container=wanted_container, rendering_method='archived')
-		else:
-			# otherwise we doa different action and want to stay on the site
-			button_handler = ButtonHandler(request, view='details.html', container=wanted_container)
-	except:
-		# we enter the site for the first time
+	# https: // stackoverflow.com/questions/5895588/django-multivaluedictkeyerror-error-how-do-i-deal-with-it
+	if request.POST.get('action', '') == 'remove':
+		# if we  want to remove the container, we need to redirect to a different site
+		button_handler = ButtonHandler(request, view='observe.html', container=wanted_container, rendering_method='archived')
+	else:
+		# otherwise we doa different action and want to stay on the site
 		button_handler = ButtonHandler(request, view='details.html', container=wanted_container)
 	return button_handler.do_button_click()
 
