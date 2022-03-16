3add36c9ed5781445f937eb238724b7e496ed878
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-03-16 10:13:04 +0100

    Added get_defaults argument

--- src/alpha_business/main.py
+++ src/alpha_business/main.py
@@ -7,15 +7,25 @@ from alpha_business.rl import training_scenario
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Customize your alpha_business experience.')
+	parser.add_argument('--datapath', type=str, help='Provide the path where alpha_business will look for and save data')
+	parser.add_argument('--get-defaults', action='store_true', help="""Default files, such as a `hyperparameter_config.json` and
+trained models will be saved to your `data_path`""")
 	parser.add_argument('-c', '--command', type=str, choices=['training', 'exampleprinter', 'agent_monitoring'],
 		default='training', help='Choose the command to run')
-	parser.add_argument('--datapath', type=str, help='Provide the path where alpha_business will look for and save data')
 
 	args = parser.parse_args()
 
-	# Manage the datapath
+	# --datapath
+	# Update the datapath if possible
 	PathManager.manage_data_path(PathManager, args.datapath)
 
+	# --get-defaults
+	if args.get_defaults:
+		# DO MAGIC HERE
+		# Any default files should be copied over to the data_path directory
+		pass
+
+	# --command
 	# Choose the file to run depending on the command
 	if args.command == 'training':
 		training_scenario.main()
