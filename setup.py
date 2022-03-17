d5c00bd1d4eb5bb7f5a95d918b634297660ce749
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-03-17 10:54:28 +0100

    Moved files

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ def package_files(directory):
 	return paths
 
 
-extra_files = package_files('src/alpha_business')
+extra_files = package_files('alpha_business')
 
 if __name__ == '__main__':
 	# package data defines folders with non-.py files which should also be included in the pip package
 	setup(
-		packages=['src'],
+		packages=['alpha_business'],
 		package_data={'': extra_files, 'alpha_business': ['default_data/*/*']}
 	)
