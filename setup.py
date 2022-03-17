f3549233a2a911eb72da3be80718b2dc3cc60f3b
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-03-17 10:06:31 +0100

    Try non-dev install of package

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 
 if __name__ == '__main__':
 	# package data defines folders with non-.py files which should also be included in the pip package
-	# in our case, this is the default data we want to provide the user with
 	setup(package_data={
 		'alpha_business': ['default_data/*/*',
 			'monitoring/data/MarketOverview_template.svg',
