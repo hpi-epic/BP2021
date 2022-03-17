732779879f29d02fa376bf4b1a63448fc3d300ee
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-03-17 11:45:54 +0100

    Correctly install :)

--- setup.py
+++ setup.py
@@ -4,17 +4,26 @@ import os
 from setuptools import setup
 
 
-def package_files(directory):
+# from: https://stackoverflow.com/questions/27664504/how-to-add-package-data-recursively-in-python-setup-py
+def package_files(directory) -> list:
+	"""
+	This will make sure that ANY file within `recommerce/` is included in the pip package.
+
+	Args:
+		directory (str): The directory to check, in our case, 'recommerce'.
+
+	Returns:
+		list: The list of files found.
+	"""
 	paths = []
 	for (path, directories, filenames) in os.walk(directory):
 		paths.extend(os.path.join('..', path, filename) for filename in filenames)
 	return paths
 
 
-extra_files = package_files('recommerce')
-
 if __name__ == '__main__':
-	# package data defines folders with non-.py files which should also be included in the pip package
+	extra_files = package_files('recommerce')
 	setup(
-		packages=['recommerce']
+		packages=['recommerce'],
+		package_data={'': extra_files}
 	)
