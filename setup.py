8a21544b7230c92dd237cd8efe0d4fd409f8f9cc
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-03-17 09:59:28 +0100

    Empty `user_path.txt`

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
 	# package data defines folders with non-.py files which should also be included in the pip package
 	# in our case, this is the default data we want to provide the user with
 	setup(package_data={
-		'alpha_business': ['default_data/*/*']
+		'alpha_business': ['default_data/*/*',
+			'monitoring/data/MarketOverview_template.svg',
+			'configuration/user_path.txt']
 	})
