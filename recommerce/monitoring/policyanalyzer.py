cace8429a55c354441b5d34dfed4fd1a090cb610
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2022-03-22 17:06:47 +0100

    satisfied linter

--- recommerce/monitoring/policyanalyzer.py
+++ recommerce/monitoring/policyanalyzer.py
@@ -1,36 +1,39 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+
 from recommerce.configuration.path_manager import PathManager
+
 
 class PolicyAnalyzer():
     def __init__(self, agent_to_analyze):
         self.agent_to_analyze = agent_to_analyze
         self.folder_path = os.path.abspath(os.path.join(PathManager.results_path, 'policyanalyzer'))
 
-    def assert_analyzed_feature_valid(self, feature):
-        assert isinstance(feature, tuple), f"{feature}: such a feature must be a tuple"
-        assert len(feature) == 3, f"{feature}: such a feature must be a triple"
-        assert isinstance(feature[0], int), f"{feature}: the first entry must be the input index to analyze"
-        assert isinstance(feature[1], str), f"{feature}: the second entry must be the description, a string"
-        assert isinstance(feature[2], range), f"{feature}: the third entry must be the search range"
+    def _assert_analyzed_feature_valid(self, feature):
+        assert isinstance(feature, tuple), f'{feature}: such a feature must be a tuple'
+        assert len(feature) == 3, f'{feature}: such a feature must be a triple'
+        assert isinstance(feature[0], int), f'{feature}: the first entry must be the input index to analyze'
+        assert isinstance(feature[1], str), f'{feature}: the second entry must be the description, a string'
+        assert isinstance(feature[2], range), f'{feature}: the third entry must be the search range'
 
-    def access_and_adjust_policy(self, policy_value, policy_access):
+    def _access_and_adjust_policy(self, policy_value, policy_access):
         policy_value = policy_value if policy_access is None else policy_value[policy_access]
         assert isinstance(policy_value, int)
         return policy_value + 1
 
-    def analyze_policy(self, base_input, analyzed_features, title="add a title here", policyaccess=None):
-        assert isinstance(base_input, np.ndarray), "base_input must be a numpy ndarray"
-        assert isinstance(analyzed_features, list), "analyzed_features must be a list containing triples"
-        assert 1 <= len(analyzed_features) and len(analyzed_features) <= 2, "you can analyze either one or two features at once"
-        assert isinstance(title, str), "title must be a string"
-        assert policyaccess is None or isinstance(policyaccess, int), "if policyaccess is not None, policyaccess must be an integer"
-        self.assert_analyzed_feature_valid(analyzed_features[0])
+    def analyze_policy(self, base_input, analyzed_features, title='add a title here', policyaccess=None):
+        assert isinstance(base_input, np.ndarray), 'base_input must be a numpy ndarray'
+        assert isinstance(analyzed_features, list), 'analyzed_features must be a list containing triples'
+        assert 1 <= len(analyzed_features) and len(analyzed_features) <= 2, 'you can analyze either one or two features at once'
+        assert isinstance(title, str), 'title must be a string'
+        assert policyaccess is None or isinstance(policyaccess, int), 'if policyaccess is not None, policyaccess must be an integer'
+        self._assert_analyzed_feature_valid(analyzed_features[0])
         if len(analyzed_features) == 2:
-            self.assert_analyzed_feature_valid(analyzed_features[1])
-            assert analyzed_features[0][0] != analyzed_features[1][0], "the two entries must analyze different features"
-            
+            self._assert_analyzed_feature_valid(analyzed_features[1])
+            assert analyzed_features[0][0] != analyzed_features[1][0], 'the two entries must analyze different features'
+
         plt.clf()
         plt.xlabel(analyzed_features[0][1])
         if len(analyzed_features) == 1:
@@ -39,7 +42,7 @@ class PolicyAnalyzer():
             for x in analyzed_features[0][2]:
                 base_input[analyzed_features[0][0]] = x
                 y = self.agent_to_analyze.policy(base_input)
-                y = self.access_and_adjust_policy(y, policyaccess)
+                y = self._access_and_adjust_policy(y, policyaccess)
                 pointsx.append(x)
                 pointsy.append(y)
 
@@ -55,14 +58,15 @@ class PolicyAnalyzer():
                     base_input[analyzed_features[0][0]] = x1
                     base_input[analyzed_features[1][0]] = x2
                     y = self.agent_to_analyze.policy(base_input)
-                    y = self.access_and_adjust_policy(y, policyaccess)
+                    y = self._access_and_adjust_policy(y, policyaccess)
                     policyval[x2 - x2base][x1 - x1base] = y
 
-            myextend = [min(analyzed_features[0][2]), max(analyzed_features[0][2]), min(analyzed_features[1][2]), max(analyzed_features[1][2])]
+            myextend = [min(analyzed_features[0][2]), max(analyzed_features[0][2]),
+                        min(analyzed_features[1][2]), max(analyzed_features[1][2])]
             p = plt.imshow(policyval, aspect='auto', origin='lower', extent=myextend)
             plt.colorbar(p)
             plt.ylabel(analyzed_features[1][1])
 
         plt.title(title)
-        underscore_title = title.replace(" ", "_")
-        plt.savefig(fname=os.path.join(self.folder_path, f"{underscore_title}.png"))
+        underscore_title = title.replace(' ', '_')
+        plt.savefig(fname=os.path.join(self.folder_path, f'{underscore_title}.png'))
--- tests/test_policyanalyzer.py
+++ tests/test_policyanalyzer.py
@@ -1,57 +1,89 @@
-from recommerce.market.linear.linear_vendors import CompetitorLinearRatio1
-from recommerce.market.circular.circular_vendors import RuleBasedCERebuyAgent
-from recommerce.rl.q_learning.q_learning_agent import QLearningCERebuyAgent
-from recommerce.rl.actorcritic.actorcritic_agent import ContinuosActorCriticAgentFixedOneStd
-from recommerce.monitoring.policyanalyzer import PolicyAnalyzer
-import numpy as np
 import os
-import recommerce.configuration.path_manager as path_manager
+
+import numpy as np
 import pytest
+
+from recommerce.market.circular.circular_vendors import RuleBasedCERebuyAgent
+from recommerce.market.linear.linear_vendors import CompetitorLinearRatio1
+from recommerce.monitoring.policyanalyzer import PolicyAnalyzer
+from recommerce.rl.actorcritic.actorcritic_agent import ContinuosActorCriticAgentFixedOneStd
+from recommerce.rl.q_learning.q_learning_agent import QLearningCERebuyAgent
 
 # The load path for the agent modelos.paths
 parameters_path = os.path.join('tests', 'test_data')
 write_to_path = os.path.join('tests', 'test_results', 'policyanalyzer')
 
+
 def test_rule_based_linear_competitor1():
     pa = PolicyAnalyzer(CompetitorLinearRatio1())
-    pa.analyze_policy(np.array([15, -1, 10]), [(1, "competitor price", range(1, 11))])
+    pa.analyze_policy(np.array([15, -1, 10]), [(1, 'competitor price', range(1, 11))])
     assert os.path.exists(os.path.join(write_to_path, 'add_a_title_here.png'))
+
 
 def test_rule_based_linear_competitor2():
     pa = PolicyAnalyzer(CompetitorLinearRatio1())
-    pa.analyze_policy(np.array([-1, -1, 10]), [(0, "own quality", range(5, 20)), (1, "competitor price", range(1, 11))], "agent's policy")
+    pa.analyze_policy(np.array([-1, -1, 10]), [(0, 'own quality', range(5, 20)), (1, 'competitor price', range(1, 11))], "agent's policy")
     assert os.path.exists(os.path.join(write_to_path, 'agent\'s_policy.png'))
+
 
 def test_rule_based_circular_competitor1():
     pa = PolicyAnalyzer(RuleBasedCERebuyAgent())
-    pa.analyze_policy(np.array([50, -1]), [(1, "self storage stock", range(0, 30))], "refurbished price only on storage", 0)
+    pa.analyze_policy(np.array([50, -1]), [(1, 'self storage stock', range(0, 30))], 'refurbished price only on storage', 0)
     assert os.path.exists(os.path.join(write_to_path, 'refurbished_price_only_on_storage.png'))
+
 
 def test_rule_based_circular_competitor2():
     pa = PolicyAnalyzer(RuleBasedCERebuyAgent())
-    pa.analyze_policy(np.array([-1, -1]), [(1, "self storage stock", range(0, 30)), (0, "in circulation", range(0, 200))], "refurbished price", 0)
+    pa.analyze_policy(
+        np.array([-1, -1]),
+        [(1, 'self storage stock', range(0, 30)),
+        (0, 'in circulation', range(0, 200))],
+        'refurbished price', 0
+    )
     assert os.path.exists(os.path.join(write_to_path, 'refurbished_price.png'))
 
+
 monopoly_test_cases = [
-    ("refurbished price", 0, "refurbished_price.png"),
-    ("new price", 1, "new_price.png"),
-    ("rebuy price", 2, "rebuy_price.png")
+    ('refurbished price', 0, 'refurbished_price.png'),
+    ('new price', 1, 'new_price.png'),
+    ('rebuy price', 2, 'rebuy_price.png')
 ]
+
+
 @pytest.mark.parametrize('title, policyaccess, expected_filename', monopoly_test_cases)
 def test_circular_monopoly_q_learning(title, policyaccess, expected_filename):
-    q_learing_agent = QLearningCERebuyAgent(2, 1000, load_path=os.path.join(parameters_path, 'CircularEconomyRebuyPriceMonopolyScenario_QLearningCERebuyAgent.dat'))
+    q_learing_agent = QLearningCERebuyAgent(
+        2, 1000, load_path=os.path.join(parameters_path,
+        'CircularEconomyRebuyPriceMonopolyScenario_QLearningCERebuyAgent.dat')
+    )
     pa = PolicyAnalyzer(q_learing_agent)
-    pa.analyze_policy(np.array([-1, -1]), [(1, "self storage stock", range(0, 30)), (0, "in circulation", range(0, 200))], title, policyaccess)
+    pa.analyze_policy(
+        np.array([-1, -1]),
+        [(1, 'self storage stock', range(0, 30)),
+        (0, 'in circulation', range(0, 200))],
+        title, policyaccess
+    )
     assert os.path.exists(os.path.join(write_to_path, expected_filename))
 
+
 one_competitor_test_cases = [
-    ("own refurbished price", 0, "own_refurbished_price.png"),
-    ("own new price", 1, "own_new_price.png"),
-    ("own rebuy price", 2, "own_rebuy_price.png")
+    ('own refurbished price', 0, 'own_refurbished_price.png'),
+    ('own new price', 1, 'own_new_price.png'),
+    ('own rebuy price', 2, 'own_rebuy_price.png')
 ]
+
+
 @pytest.mark.parametrize('title, policyaccess, expected_filename', one_competitor_test_cases)
 def test_circular_duopol_continuos_actorcritic(title, policyaccess, expected_filename):
-    q_learing_agent = ContinuosActorCriticAgentFixedOneStd(6, 3, load_path=os.path.join(parameters_path, 'actor_parametersCircularEconomyRebuyPriceOneCompetitor_ContinuosActorCriticAgentFixedOneStd.dat'))
+    q_learing_agent = ContinuosActorCriticAgentFixedOneStd(
+        6, 3, load_path=os.path.join(parameters_path,
+            'actor_parametersCircularEconomyRebuyPriceOneCompetitor_ContinuosActorCriticAgentFixedOneStd.dat')
+    )
     pa = PolicyAnalyzer(q_learing_agent)
-    pa.analyze_policy(np.array([75, 10, -1, -1, 2, 12]), [(1, "competitor's refurbished price", range(0, 10)), (0, "competitor's new price", range(0, 10))], title, policyaccess)
+    pa.analyze_policy(
+        np.array([75, 10, -1, -1, 2, 12]),
+        [(1, "competitor's refurbished price", range(0, 10)),
+        (0, "competitor's new price", range(0, 10))],
+        title, policyaccess
+    )
     assert os.path.exists(os.path.join(write_to_path, expected_filename))
