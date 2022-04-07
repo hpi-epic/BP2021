cf57fd733a9510cc568dfacfb78bd9e66c085a5f
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-07 17:12:58 +0200

    More tests

--- tests/test_config_validation.py
+++ tests/test_config_validation.py
@@ -25,7 +25,42 @@ validate_config_valid_final_testcases = [
 
 
 @pytest.mark.parametrize('config', validate_config_valid_final_testcases)
-def test_validate_config_valid_final(config):
+def test_validate_config_valid_combined_final(config):
 	# If the config is valid, the first member of the tuple returned will be True
 	validate_status, validate_data = config_validation.validate_config(config, True)
 	assert validate_status, validate_data
+
+
+# These testcases do not cover everything, nor should they, there are simply too many combinations
+validate_config_valid_not_final_testcases = [
+	ut_t.create_combined_mock_dict(
+		hyperparameter=ut_t.create_hyperparameter_mock_dict(
+			rl=ut_t.remove_key('learning_rate', ut_t.create_hyperparameter_mock_dict_rl(gamma=0.5)))),
+	ut_t.create_combined_mock_dict(
+		hyperparameter=ut_t.create_hyperparameter_mock_dict(
+			rl=ut_t.remove_key('epsilon_start', ut_t.remove_key('learning_rate', ut_t.create_hyperparameter_mock_dict_rl())))),
+	ut_t.create_combined_mock_dict(environment=ut_t.remove_key('task', ut_t.create_environment_mock_dict())),
+	ut_t.create_combined_mock_dict(environment=ut_t.remove_key('agents', ut_t.remove_key('task', ut_t.create_environment_mock_dict()))),
+] + validate_config_valid_final_testcases
+
+
+@pytest.mark.parametrize('config', validate_config_valid_not_final_testcases)
+def test_validate_config_valid_combined_not_final(config):
+	# If the config is valid, the first member of the tuple returned will be True
+	validate_status, validate_data = config_validation.validate_config(config, False)
+	assert validate_status, validate_data
+
+
+test_validate_config_not_final_still_needs_both_testcases = [
+	(ut_t.create_combined_mock_dict(hyperparameter=None), True),
+	(ut_t.create_combined_mock_dict(environment=None), True),
+	(ut_t.create_combined_mock_dict(hyperparameter=None), False),
+	(ut_t.create_combined_mock_dict(environment=None), False)
+]
+
+
+@pytest.mark.parametrize('config, is_final', test_validate_config_not_final_still_needs_both_testcases)
+def test_validate_config_one_top_key_missing(config, is_final):
+	validate_status, validate_data = config_validation.validate_config(config, is_final)
+	assert not validate_status
+	assert 'If your config contains one of "environment" or "hyperparameter" it must also contain the other' == validate_data
