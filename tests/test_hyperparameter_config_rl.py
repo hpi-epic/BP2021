a8adac88afb66d02a859597dc94aaf4e8dc22360
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-07 15:00:40 +0200

    Adapted to new mock format

--- tests/test_hyperparameter_config_rl.py
+++ tests/test_hyperparameter_config_rl.py
@@ -1,3 +1,4 @@
+import json
 from importlib import reload
 from unittest.mock import mock_open, patch
 
@@ -27,9 +28,9 @@ def import_config() -> hyperparameter_config.HyperparameterConfig:
 # https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth
 # Test that checks if the config.json is read correctly
 def test_reading_file_values():
-	json = ut_t.create_hyperparameter_mock_json()
-	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
-		ut_t.check_mock_file(mock_file, json)
+	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict())
+	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
+		ut_t.check_mock_file(mock_file, mock_json)
 
 		config = import_config()
 
@@ -44,9 +45,9 @@ def test_reading_file_values():
 		assert config.epsilon_final == 0.1
 
 	# Test a second time with other values to ensure that the values are read correctly
-	json2 = ut_t.create_hyperparameter_mock_json(rl=ut_t.create_hyperparameter_mock_json_rl(learning_rate='1e-4'))
-	with patch('builtins.open', mock_open(read_data=json2)) as mock_file:
-		ut_t.check_mock_file(mock_file, json2)
+	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict(rl=ut_t.create_hyperparameter_mock_dict_rl(learning_rate=1e-4)))
+	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
+		ut_t.check_mock_file(mock_file, mock_json)
 
 		config = import_config()
 
@@ -55,35 +56,37 @@ def test_reading_file_values():
 
 # The following variables are input mock-json strings for the test_invalid_values test
 # These tests have invalid values in their input file, the import should throw a specific error message
-learning_rate_larger_one = (ut_t.create_hyperparameter_mock_json_rl(learning_rate='1.5'),
+learning_rate_larger_one = (ut_t.create_hyperparameter_mock_dict_rl(learning_rate=1.5),
 	'learning_rate should be between 0 and 1 (excluded)')
-negative_learning_rate = (ut_t.create_hyperparameter_mock_json_rl(learning_rate='0'), 'learning_rate should be between 0 and 1 (excluded)')
-large_gamma = (ut_t.create_hyperparameter_mock_json_rl(gamma='1.0'), 'gamma should be between 0 (included) and 1 (excluded)')
-negative_gamma = ((ut_t.create_hyperparameter_mock_json_rl(gamma='-1.0'), 'gamma should be between 0 (included) and 1 (excluded)'))
-negative_batch_size = (ut_t.create_hyperparameter_mock_json_rl(batch_size='-5'), 'batch_size should be greater than 0')
-negative_replay_size = (ut_t.create_hyperparameter_mock_json_rl(replay_size='-5'),
+negative_learning_rate = (ut_t.create_hyperparameter_mock_dict_rl(learning_rate=0), 'learning_rate should be between 0 and 1 (excluded)')
+large_gamma = (ut_t.create_hyperparameter_mock_dict_rl(gamma=1.0), 'gamma should be between 0 (included) and 1 (excluded)')
+negative_gamma = ((ut_t.create_hyperparameter_mock_dict_rl(gamma=-1.0), 'gamma should be between 0 (included) and 1 (excluded)'))
+negative_batch_size = (ut_t.create_hyperparameter_mock_dict_rl(batch_size=-5), 'batch_size should be greater than 0')
+negative_replay_size = (ut_t.create_hyperparameter_mock_dict_rl(replay_size=-5),
 	'replay_size should be greater than 0')
-negative_sync_target_frames = (ut_t.create_hyperparameter_mock_json_rl(sync_target_frames='-5'),
+negative_sync_target_frames = (ut_t.create_hyperparameter_mock_dict_rl(sync_target_frames=-5),
 	'sync_target_frames should be greater than 0')
-negative_replay_start_size = (ut_t.create_hyperparameter_mock_json_rl(replay_start_size='-5'), 'replay_start_size should be greater than 0')
-negative_epsilon_decay_last_frame = (ut_t.create_hyperparameter_mock_json_rl(epsilon_decay_last_frame='-5'),
+negative_replay_start_size = (ut_t.create_hyperparameter_mock_dict_rl(replay_start_size=-5), 'replay_start_size should be greater than 0')
+negative_epsilon_decay_last_frame = (ut_t.create_hyperparameter_mock_dict_rl(epsilon_decay_last_frame=-5),
 	'epsilon_decay_last_frame should not be negative')
 
 
 # These tests are missing a line in the config file, the import should throw a specific error message
-missing_gamma = (ut_t.remove_line(0, ut_t.create_hyperparameter_mock_json_rl()), 'your config_rl is missing gamma')
-missing_batch_size = (ut_t.remove_line(1, ut_t.create_hyperparameter_mock_json_rl()), 'your config_rl is missing batch_size')
-missing_replay_size = (ut_t.remove_line(2, ut_t.create_hyperparameter_mock_json_rl()), 'your config_rl is missing replay_size')
-missing_learning_rate = (ut_t.remove_line(3, ut_t.create_hyperparameter_mock_json_rl()),
+missing_gamma = (ut_t.remove_key('gamma', ut_t.create_hyperparameter_mock_dict_rl()), 'your config_rl is missing gamma')
+missing_batch_size = (ut_t.remove_key('batch_size', ut_t.create_hyperparameter_mock_dict_rl()), 'your config_rl is missing batch_size')
+missing_replay_size = (ut_t.remove_key('replay_size', ut_t.create_hyperparameter_mock_dict_rl()), 'your config_rl is missing replay_size')
+missing_learning_rate = (ut_t.remove_key('learning_rate', ut_t.create_hyperparameter_mock_dict_rl()),
 	'your config_rl is missing learning_rate')
-missing_sync_target_frames = (ut_t.remove_line(4, ut_t.create_hyperparameter_mock_json_rl()),
+missing_sync_target_frames = (ut_t.remove_key('sync_target_frames', ut_t.create_hyperparameter_mock_dict_rl()),
 	'your config_rl is missing sync_target_frames')
-missing_replay_start_size = (ut_t.remove_line(5, ut_t.create_hyperparameter_mock_json_rl()),
+missing_replay_start_size = (ut_t.remove_key('replay_start_size', ut_t.create_hyperparameter_mock_dict_rl()),
 	'your config_rl is missing replay_start_size')
-missing_epsilon_decay_last_frame = (ut_t.remove_line(6, ut_t.create_hyperparameter_mock_json_rl()),
+missing_epsilon_decay_last_frame = (ut_t.remove_key('epsilon_decay_last_frame', ut_t.create_hyperparameter_mock_dict_rl()),
 	'your config_rl is missing epsilon_decay_last_frame')
-missing_epsilon_start = (ut_t.remove_line(7, ut_t.create_hyperparameter_mock_json_rl()), 'your config_rl is missing epsilon_start')
-missing_epsilon_final = (ut_t.remove_line(8, ut_t.create_hyperparameter_mock_json_rl()), 'your config_rl is missing epsilon_final')
+missing_epsilon_start = (ut_t.remove_key('epsilon_start', ut_t.create_hyperparameter_mock_dict_rl()),
+	'your config_rl is missing epsilon_start')
+missing_epsilon_final = (ut_t.remove_key('epsilon_final', ut_t.create_hyperparameter_mock_dict_rl()),
+	'your config_rl is missing epsilon_final')
 
 
 invalid_values_testcases = [
@@ -111,9 +114,9 @@ invalid_values_testcases = [
 # Test that checks that an invalid/broken config.json gets detected correctly
 @pytest.mark.parametrize('rl_json, expected_message', invalid_values_testcases)
 def test_invalid_values(rl_json, expected_message):
-	json = ut_t.create_hyperparameter_mock_json(rl=rl_json)
-	with patch('builtins.open', mock_open(read_data=json)) as mock_file:
-		ut_t.check_mock_file(mock_file, json)
+	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict(rl=rl_json))
+	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
+		ut_t.check_mock_file(mock_file, mock_json)
 		with pytest.raises(AssertionError) as assertion_message:
 			import_config()
 		assert expected_message in str(assertion_message.value)
