813359b14a85ba5648757440cd20f74e02b19f86
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2022-04-07 18:20:48 +0200

    fixed model file ending bug

--- recommerce/monitoring/agent_monitoring/am_configuration.py
+++ recommerce/monitoring/agent_monitoring/am_configuration.py
@@ -52,7 +52,7 @@ class Configurator():
 		Returns:
 			str: The full path to the modelfile.
 		"""
-		assert model_name.endswith('.dat') and not model_name.endswith('.zip'), f'Modelfiles must end in .dat or .zip: {model_name}'
+		assert model_name.endswith('.dat') or model_name.endswith('.zip'), f'Modelfiles must end in .dat or .zip: {model_name}'
 		full_path = os.path.join(PathManager.data_path, model_name)
 		assert os.path.exists(full_path), f'the specified modelfile does not exist: {full_path}'
 		return full_path
