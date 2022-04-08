d1aed39d44e01357f77896ec2a6ea7e232322546
NikkelM <nikkel.mollenhauer@student.hpi.de>
2022-04-08 10:34:58 +0200

    More tests

--- tests/test_config_validation.py
+++ tests/test_config_validation.py
@@ -31,7 +31,7 @@ validate_config_valid_combined_final_testcases = [
 def test_validate_config_valid_combined_final(config):
 	# If the config is valid, the first member of the tuple returned will be True
 	validate_status, validate_data = config_validation.validate_config(config, True)
-	assert validate_status
+	assert validate_status, validate_data
 	assert isinstance(validate_data, tuple)
 	assert 'rl' in validate_data[0]
 	assert 'sim_market' in validate_data[0]
@@ -74,7 +74,7 @@ test_validate_config_one_top_key_missing_testcases = [
 @pytest.mark.parametrize('config, is_final', test_validate_config_one_top_key_missing_testcases)
 def test_validate_config_one_top_key_missing(config, is_final):
 	validate_status, validate_data = config_validation.validate_config(config, is_final)
-	assert not validate_status
+	assert not validate_status, validate_data
 	assert 'If your config contains one of "environment" or "hyperparameter" it must also contain the other' == validate_data
 
 
@@ -89,12 +89,84 @@ def test_validate_config_too_many_keys(is_final):
 	test_config = ut_t.create_combined_mock_dict()
 	test_config['additional_key'] = "this should'nt be allowed"
 	validate_status, validate_data = config_validation.validate_config(test_config, is_final)
-	assert not validate_status
+	assert not validate_status, validate_data
 	assert 'Your config should not contain keys other than "environment" and "hyperparameter"' == validate_data
 ##########
 # End of tests with already combined configs (== hyperparameter and/or environment key on the top-level)
 ##########
 
 
-def test_validate_config():
-	pass
+##########
+# Tests without the already split top-level (config keys are mixed and need to be matched)
+##########
+# These are singular dicts that will get combined for the actual testcases
+test_validate_config_valid_not_final_dicts = [
+	{
+		'rl': {
+			'gamma': 0.5,
+			'epsilon_start': 0.9
+		}
+	},
+	{
+		'sim_market': {
+			'max_price': 40
+		}
+	},
+	{
+		'task': 'training'
+	},
+	{
+		'marketplace': 'recommerce.market.circular.circular_sim_market.CircularEconomyRebuyPriceMonopolyScenario'
+	},
+	{
+		'agents': [
+			{
+				'name': 'Rule_Based Agent',
+				'agent_class': 'recommerce.market.circular.circular_vendors.RuleBasedCERebuyAgent',
+				'argument': ''
+			},
+			{
+				'name': 'CE Rebuy Agent (QLearning)',
+				'agent_class': 'recommerce.rl.q_learning.q_learning_agent.QLearningCERebuyAgent',
+				'argument': 'CircularEconomyRebuyPriceMonopolyScenario_QLearningCERebuyAgent.dat'
+			}
+		]
+	},
+	{
+		'agents': [
+			{
+				'name': 'Rule_Based Agent',
+				'agent_class': 'recommerce.market.circular.circular_vendors.RuleBasedCERebuyAgent',
+				'argument': ''
+			}
+		]
+	}
+]
+
+
+# get all combinations of the dicts defined above to mix and match as much as possible
+test_validate_config_valid_not_final_testcases = [
+	{**dict1, **dict2} for dict1 in test_validate_config_valid_not_final_dicts for dict2 in test_validate_config_valid_not_final_dicts
+]
+
+
+@pytest.mark.parametrize('config', test_validate_config_valid_not_final_testcases)
+def test_validate_config_valid_not_final(config):
+	validate_status, validate_data = config_validation.validate_config(config, False)
+	assert validate_status, f'Test failed with error: {validate_data} on config: {config}'
+
+
+test_validate_config_valid_final_testcases = [
+	{**ut_t.create_hyperparameter_mock_dict(), **ut_t.create_environment_mock_dict()},
+	{**ut_t.create_hyperparameter_mock_dict(rl=ut_t.create_hyperparameter_mock_dict_rl(gamma=0.2)), **ut_t.create_environment_mock_dict()},
+	{**ut_t.create_hyperparameter_mock_dict(), **ut_t.create_environment_mock_dict(episodes=20)}
+]
+
+
+@pytest.mark.parametrize('config', test_validate_config_valid_final_testcases)
+def test_validate_config_valid_final(config):
+	validate_status, validate_data = config_validation.validate_config(config, True)
+	assert validate_status, f'Test failed with error: {validate_data} on config: {config}'
+	assert 'rl' in validate_data[0]
+	assert 'sim_market' in validate_data[0]
+	assert 'agents' in validate_data[1]
