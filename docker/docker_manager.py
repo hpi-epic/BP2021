1ee8f1f4fba9fb9a4db442d5500fbf3a90afa9bc
Judith <39854388+felix-20@users.noreply.github.com>
2022-04-13 08:23:50 +0200

    activate `use_gpu`

--- docker/docker_manager.py
+++ docker/docker_manager.py
@@ -6,6 +6,8 @@ import time
 from itertools import count, filterfalse
 from types import GeneratorType
 
+from torch.cuda import is_available
+
 import docker
 from docker.models.containers import Container
 
@@ -101,7 +103,8 @@ class DockerManager():
 		all_container_infos = []
 		for _ in range(count):
 			# start a container for the image of the requested command
-			container_info: DockerInfo = self._create_container(command_id, config, use_gpu=False)
+			print('cuda?', is_available())
+			container_info: DockerInfo = self._create_container(command_id, config, use_gpu=is_available())
 			if 'Image not found' in container_info.status or container_info.data is False:
 				# something is wrong with our container
 				self.remove_container(container_info.id)
