b843e0df1cc8048091a7b7cd7f671ecb842e795a
Judith <39854388+felix-20@users.noreply.github.com>
2022-04-25 16:28:07 +0200

    basic logging on the webserver site

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -120,3 +120,33 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'root': {'level': 'INFO', 'handlers': ['file']},
+	'handlers': {
+		'file': {
+			'level': 'INFO',
+			'class': 'logging.FileHandler',
+			'filename': './django.log',
+			'formatter': 'app',
+		},
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['file'],
+			'level': 'INFO',
+			'propagate': True
+		},
+	},
+	'formatters': {
+		'app': {
+			'format': (
+				u'%(asctime)s [%(levelname)-8s] '
+				'(%(module)s.%(funcName)s) %(message)s'
+			),
+			'datefmt': '%Y-%m-%d %H:%M:%S',
+		},
+	},
+}
