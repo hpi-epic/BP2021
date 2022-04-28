ee047ae43f8d092a6136a0edac9bd935ca1ed849
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2022-04-28 17:16:08 +0200

    fixed test_stable_baselines

--- tests/test_stable_baselines_training.py
+++ tests/test_stable_baselines_training.py
@@ -2,33 +2,36 @@ import pytest
 
 import recommerce.market.circular.circular_sim_market as circular_market
 import recommerce.rl.stable_baselines.stable_baselines_model as sb_model
+from recommerce.configuration.hyperparameter_config import HyperparameterConfigLoader, HyperparameterConfig
+
+config_hyperparameter: HyperparameterConfig = HyperparameterConfigLoader.load('hyperparameter_config')
 
 
 @pytest.mark.training
 @pytest.mark.slow
 def test_ddpg_training():
-	sb_model.StableBaselinesDDPG(circular_market.CircularEconomyRebuyPriceDuopoly(True)).train_agent(1500, 30)
+	sb_model.StableBaselinesDDPG(config_hyperparameter, circular_market.CircularEconomyRebuyPriceDuopoly(config=config_hyperparameter, support_continuous_action_space=True)).train_agent(1500, 30)
 
 
 @pytest.mark.training
 @pytest.mark.slow
 def test_td3_training():
-	sb_model.StableBaselinesTD3(circular_market.CircularEconomyRebuyPriceDuopoly(True)).train_agent(1500, 30)
+	sb_model.StableBaselinesTD3(config_hyperparameter, circular_market.CircularEconomyRebuyPriceDuopoly(config=config_hyperparameter, support_continuous_action_space=True)).train_agent(1500, 30)
 
 
 @pytest.mark.training
 @pytest.mark.slow
 def test_a2c_training():
-	sb_model.StableBaselinesA2C(circular_market.CircularEconomyRebuyPriceDuopoly(True)).train_agent(1500, 30)
+	sb_model.StableBaselinesA2C(config_hyperparameter, circular_market.CircularEconomyRebuyPriceDuopoly(config=config_hyperparameter, support_continuous_action_space=True)).train_agent(1500, 30)
 
 
 @pytest.mark.training
 @pytest.mark.slow
 def test_ppo_training():
-	sb_model.StableBaselinesPPO(circular_market.CircularEconomyRebuyPriceDuopoly(True)).train_agent(1500, 30)
+	sb_model.StableBaselinesPPO(config_hyperparameter, circular_market.CircularEconomyRebuyPriceDuopoly(config=config_hyperparameter, support_continuous_action_space=True)).train_agent(1500, 30)
 
 
 @pytest.mark.training
 @pytest.mark.slow
 def test_sac_training():
-	sb_model.StableBaselinesSAC(circular_market.CircularEconomyRebuyPriceDuopoly(True)).train_agent(1500, 30)
+	sb_model.StableBaselinesSAC(config_hyperparameter, circular_market.CircularEconomyRebuyPriceDuopoly(config=config_hyperparameter, support_continuous_action_space=True)).train_agent(1500, 30)
