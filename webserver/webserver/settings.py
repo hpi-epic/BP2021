72335c0606e8903159efb6b51dfa51fdb9f4a3ba
Judith <39854388+felix-20@users.noreply.github.com>
2022-05-03 11:06:06 +0200

    environment variables will be read from local `.env` file

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -20,7 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']  # '*&qf$4_z-z(0+f@ngx++bjx7i-v-^6lj9wd^q8q9zmbhj6na7s'
+try:
+	with open('./.env.txt', 'r') as file:
+		lines = file.readlines()
+		SECRET_KEY = lines[0]
+except FileNotFoundError:
+	print('No .env file found, using environment variable instead.')
+	try:
+		SECRET_KEY = os.environ['SECRET_KEY']
+	except KeyError:
+		print('You either need to have a .env file or the environment variable SECRET_KEY set. Exiting')
+		exit(1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
@@ -131,33 +141,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-LOGGING = {
-	'version': 1,
-	'disable_existing_loggers': False,
-	'root': {'level': 'INFO', 'handlers': ['file']},
-	'handlers': {
-		'file': {
-			'level': 'INFO',
-			'class': 'logging.FileHandler',
-			'filename': './django.log',
-			'formatter': 'app',
-		},
-	},
-	'loggers': {
-		'django': {
-			'handlers': ['file'],
-			'level': 'INFO',
-			'propagate': True
-		},
-	},
-	'formatters': {
-		'app': {
-			'format': (
-				u'%(asctime)s [%(levelname)-8s] '
-				'(%(module)s.%(funcName)s) %(message)s'
-			),
-			'datefmt': '%Y-%m-%d %H:%M:%S',
-		},
-	},
-}
