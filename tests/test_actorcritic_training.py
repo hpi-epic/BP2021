b34da9f48369cae3fadd6a77f77004e05a817333
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2022-05-13 10:51:15 +0200

    Reduced test runtime

--- tests/test_actorcritic_training.py
+++ tests/test_actorcritic_training.py
@@ -1,13 +1,10 @@
 import pytest
 import utils_tests as ut_t
-from attrdict import AttrDict
 
 import recommerce.market.circular.circular_sim_market as circular_market
 import recommerce.market.linear.linear_sim_market as linear_market
 import recommerce.rl.actorcritic.actorcritic_agent as actorcritic_agent
 from recommerce.rl.actorcritic.actorcritic_training import ActorCriticTrainer
-
-config_hyperparameter: AttrDict = ut_t.mock_config_hyperparameter()
 
 test_scenarios = [
 	(linear_market.LinearEconomyDuopoly, actorcritic_agent.DiscreteActorCriticAgent, True),
@@ -32,7 +29,9 @@ test_scenarios = [
 @pytest.mark.slow
 @pytest.mark.parametrize('market_class, agent_class, verbose', test_scenarios)
 def test_training_configurations(market_class, agent_class, verbose):
-	ActorCriticTrainer(market_class, agent_class, config=ut_t.mock_config_hyperparameter()).train_agent(
+	mock_config = ut_t.mock_config_hyperparameter()
+	mock_config.batch_size = 8
+	ActorCriticTrainer(market_class, agent_class, config=mock_config).train_agent(
 		verbose=verbose,
 		number_of_training_steps=120,
 		total_envs=64)
