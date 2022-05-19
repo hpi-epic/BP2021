a37f3844dbacc85286c3d1a20fd6db42001b5434
Judith <39854388+felix-20@users.noreply.github.com>
2022-05-19 09:52:34 +0200

    fix error

--- docker_api/silent_starter.py
+++ docker_api/silent_starter.py
@@ -170,9 +170,4 @@ while True:
 			logging.info('done working')
 		print(f'Not working, it is {datetime.datetime.now().time()}')
 		first_time = True
-	print('*', running_container)
-	for i in range(10):
-		if running_container:
-			print(send_get_request('logs', running_container[0]))
-		time.sleep(5)
 	time.sleep(305)
