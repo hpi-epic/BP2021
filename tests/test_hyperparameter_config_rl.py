3fded29c003ac3f8c59058e9390fe00ab7b499f8
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2022-05-26 17:25:10 +0200

    reintroduced test_hyperparameter_config_market

--- tests/test_hyperparameter_config_rl.py
+++ tests/test_hyperparameter_config_rl.py
@@ -12,81 +12,19 @@ from recommerce.configuration.path_manager import PathManager
 rl_config_file = os.path.join(PathManager.user_path, 'configuration_files', 'rl_config.json')
 
 
-# def teardown_module(module):
-# 	print('***TEARDOWN***')
-# 	reload(hyperparameter_config)
+# Test that checks if the config.json is read correctly
+def test_reading_file_values():
+	config = hyperparameter_config.HyperparameterConfigLoader.load('rl_config')
 
-
-# ######
-# # General tests for the HyperParameter parent class
-# #####
-# get_required_fields_valid_testcases = [
-# 	('top-dict', {'rl': True, 'sim_market': True}),
-# 	('rl', {
-# 				'gamma': False,
-# 				'batch_size': False,
-# 				'replay_size': False,
-# 				'learning_rate': False,
-# 				'sync_target_frames': False,
-# 				'replay_start_size': False,
-# 				'epsilon_decay_last_frame': False,
-# 				'epsilon_start': False,
-# 				'epsilon_final': False
-# 			}),
-# 	('sim_market', {
-# 				'max_storage': False,
-# 				'episode_length': False,
-# 				'max_price': False,
-# 				'max_quality': False,
-# 				'number_of_customers': False,
-# 				'production_price': False,
-# 				'storage_cost_per_product': False
-# 			})
-# ]
-
-
-# @pytest.mark.parametrize('level, expected_dict', get_required_fields_valid_testcases)
-# def test_get_required_fields_valid(level, expected_dict):
-# 	fields = hyperparameter_config.HyperparameterConfigValidator.get_required_fields(level)
-# 	assert fields == expected_dict
-
-
-# def test_get_required_fields_invalid():
-# 	with pytest.raises(AssertionError) as error_message:
-# 		hyperparameter_config.HyperparameterConfigValidator.get_required_fields('wrong_key')
-# 	assert 'The given level does not exist in a hyperparameter-config: wrong_key' in str(error_message.value)
-# ######
-# # End general tests
-# #####
-
-
-# # mock format taken from:
-# # https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth
-# # Test that checks if the config.json is read correctly
-# def test_reading_file_values():
-# 	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict())
-# 	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, mock_json)
-# 		config = hyperparameter_config.HyperparameterConfigLoader.load('hyperparameter_config')
-
-# 		assert config.gamma == 0.99
-# 		assert config.batch_size == 32
-# 		assert config.replay_size == 500
-# 		assert config.learning_rate == 1e-6
-# 		assert config.sync_target_frames == 10
-# 		assert config.replay_start_size == 100
-# 		assert config.epsilon_decay_last_frame == 400
-# 		assert config.epsilon_start == 1.0
-# 		assert config.epsilon_final == 0.1
-
-# 	# Test a second time with other values to ensure that the values are read correctly
-# 	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict(rl=ut_t.create_hyperparameter_mock_dict_rl(learning_rate=1e-4)))
-# 	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, mock_json)
-
-# 		config = hyperparameter_config.HyperparameterConfigLoader.load('hyperparameter_config')
-
-# 		assert config.learning_rate == 1e-4
+	assert config.gamma == 0.99
+	assert config.batch_size == 8
+	assert config.replay_size == 350
+	assert config.learning_rate == 1e-6
+	assert config.sync_target_frames == 35
+	assert config.replay_start_size == 20
+	assert config.epsilon_decay_last_frame == 400
+	assert config.epsilon_start == 1.0
+	assert config.epsilon_final == 0.1
 
 
 # The following variables are input mock-json strings for the test_invalid_values test
--- tests/test_hyperparameter_config_sim_market.py
+++ tests/test_hyperparameter_config_sim_market.py
@@ -1,109 +1,81 @@
-# import json
-# from importlib import reload
-# from unittest.mock import mock_open, patch
+import json
+import os
+from unittest.mock import mock_open, patch
 
-# from recommerce.configuration.hyperparameter_config import HyperparameterConfigLoader
-# import pytest
-# import utils_tests as ut_t
+import pytest
+import utils_tests as ut_t
 
-# import recommerce.configuration.hyperparameter_config as hyperparameter_config
+from recommerce.configuration.hyperparameter_config import HyperparameterConfigLoader
+from recommerce.configuration.path_manager import PathManager
 
-
-# def teardown_module(module):
-# 	print('***TEARDOWN***')
-# 	reload(hyperparameter_config)
+market_config_file = os.path.join(PathManager.user_path, 'configuration_files', 'market_config.json')
 
 
-# # mock format taken from:
-# # https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth
-# # Test that checks if the config.json is read correctly
-# def test_reading_file_values():
-# 	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict(sim_market=HyperparameterConfigLoader.load('market_config')))
-# 	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, mock_json)
+# Test that checks if the config.json is read correctly
+def test_reading_file_values():
+	config = HyperparameterConfigLoader.load('market_config')
 
-# 		config = hyperparameter_config.HyperparameterConfigLoader.load('hyperparameter_config')
-
-# 		assert config.max_storage == 100
-# 		assert config.episode_length == 25
-# 		assert config.max_price == 10
-# 		assert config.max_quality == 50
-# 		assert config.number_of_customers == 10
-# 		assert config.production_price == 3
-# 		assert config.storage_cost_per_product == 0.1
-
-# 	# Test a second time with other values to ensure, that the values are read correctly
-# 	mock_json = json.dumps(ut_t.create_hyperparameter_mock_dict(
-# 		sim_market=ut_t.create_hyperparameter_mock_dict_sim_market(50, 50, 50, 80, 20, 10, 0.7)))
-# 	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, mock_json)
-
-# 		config = hyperparameter_config.HyperparameterConfigLoader.load('hyperparameter_config')
-
-# 		assert config.max_storage == 50
-# 		assert config.episode_length == 50
-# 		assert config.max_price == 50
-# 		assert config.max_quality == 80
-# 		assert config.number_of_customers == 20
-# 		assert config.production_price == 10
-# 		assert config.storage_cost_per_product == 0.7
+	assert config.max_storage == 100
+	assert config.episode_length == 50
+	assert config.max_price == 10
+	assert config.max_quality == 50
+	assert config.number_of_customers == 20
+	assert config.production_price == 3
+	assert config.storage_cost_per_product == 0.1
 
 
-# # The following variables are input mock-json strings for the test_invalid_values test
-# # These tests have invalid values in their input file, the import should throw a specific error message
-# odd_number_of_customers = (ut_t.create_hyperparameter_mock_dict_sim_market(number_of_customers=21),
-# 	'number_of_customers should be even and positive')
-# negative_number_of_customers = (ut_t.create_hyperparameter_mock_dict_sim_market(10, 50, 50, 80, -10, 10, 0.15),
-# 	'number_of_customers should be even and positive')
-# prod_price_higher_max_price = (ut_t.create_hyperparameter_mock_dict_sim_market(10, 50, 10, 80, 20, 50, 0.15),
-# 	'production_price needs to be smaller than max_price and >=0')
-# negative_production_price = (ut_t.create_hyperparameter_mock_dict_sim_market(10, 50, 50, 80, 20, -10, 0.15),
-# 	'production_price needs to be smaller than max_price and >=0')
-# negative_max_quality = (ut_t.create_hyperparameter_mock_dict_sim_market(10, 20, 15, -80, 30, 5, 0.15),
-# 	'max_quality should be positive')
-# non_negative_storage_cost = (ut_t.create_hyperparameter_mock_dict_sim_market(10, 20, 15, 80, 30, 5, -3.5),
-# 	'storage_cost_per_product should be non-negative')
+# The following variables are input mock-json strings for the test_invalid_values test
+# These tests have invalid values in their input file, the import should throw a specific error message
+odd_number_of_customers = (ut_t.replace_field_in_dict(ut_t.load_json(market_config_file), 'number_of_customers', 21),
+	'number_of_customers should be even and positive')
+negative_number_of_customers = (ut_t.replace_field_in_dict(ut_t.load_json(market_config_file), 'number_of_customers', -10),
+	'number_of_customers should be even and positive')
+negative_production_price = (ut_t.replace_field_in_dict(ut_t.load_json(market_config_file), 'production_price', -10),
+	'production_price should be non-negative')
+negative_max_quality = (ut_t.replace_field_in_dict(ut_t.load_json(market_config_file), 'max_quality', -80),
+	'max_quality should be positive')
+non_negative_storage_cost = (ut_t.replace_field_in_dict(ut_t.load_json(market_config_file), 'storage_cost_per_product', -3.5),
+	'storage_cost_per_product should be non-negative')
 
-# # These tests are missing a line in the config file, the import should throw a specific error message
-# missing_max_storage = (ut_t.remove_key('max_storage', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing max_storage')
-# missing_episode_length = (ut_t.remove_key('episode_length', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing episode_length')
-# missing_max_price = (ut_t.remove_key('max_price', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing max_price')
-# missing_max_quality = (ut_t.remove_key('max_quality', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing max_quality')
-# missing_number_of_customers = (ut_t.remove_key('number_of_customers', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing number_of_customers')
-# missing_production_price = (ut_t.remove_key('production_price', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing production_price')
-# missing_storage_cost = (ut_t.remove_key('storage_cost_per_product', HyperparameterConfigLoader.load('market_config')),
-# 	'your config is missing storage_cost_per_product')
+# These tests are missing a line in the config file, the import should throw a specific error message
+missing_max_storage = (ut_t.remove_key('max_storage', ut_t.load_json(market_config_file)),
+	'your config is missing max_storage')
+missing_episode_length = (ut_t.remove_key('episode_length', ut_t.load_json(market_config_file)),
+	'your config is missing episode_length')
+missing_max_price = (ut_t.remove_key('max_price', ut_t.load_json(market_config_file)),
+	'your config is missing max_price')
+missing_max_quality = (ut_t.remove_key('max_quality', ut_t.load_json(market_config_file)),
+	'your config is missing max_quality')
+missing_number_of_customers = (ut_t.remove_key('number_of_customers', ut_t.load_json(market_config_file)),
+	'your config is missing number_of_customers')
+missing_production_price = (ut_t.remove_key('production_price', ut_t.load_json(market_config_file)),
+	'your config is missing production_price')
+missing_storage_cost = (ut_t.remove_key('storage_cost_per_product', ut_t.load_json(market_config_file)),
+	'your config is missing storage_cost_per_product')
 
-# # All pairs concerning themselves with invalid config.json values should be added to this array to get tested in test_invalid_values
-# invalid_values_testcases = [
-# 	odd_number_of_customers,
-# 	negative_number_of_customers,
-# 	prod_price_higher_max_price,
-# 	negative_production_price,
-# 	negative_max_quality,
-# 	non_negative_storage_cost,
-# 	missing_max_storage,
-# 	missing_episode_length,
-# 	missing_max_price,
-# 	missing_max_quality,
-# 	missing_number_of_customers,
-# 	missing_production_price,
-# 	missing_storage_cost
-# ]
+# All pairs concerning themselves with invalid config.json values should be added to this array to get tested in test_invalid_values
+invalid_values_testcases = [
+	odd_number_of_customers,
+	negative_number_of_customers,
+	negative_production_price,
+	negative_max_quality,
+	non_negative_storage_cost,
+	missing_max_storage,
+	missing_episode_length,
+	missing_max_price,
+	missing_max_quality,
+	missing_number_of_customers,
+	missing_production_price,
+	missing_storage_cost
+]
 
 
-# # Test that checks that an invalid/broken config.json gets detected correctly
-# @pytest.mark.parametrize('config_market, expected_message', invalid_values_testcases)
-# def test_invalid_values(config_market, expected_message):
-# 	mock_json = json.dumps(config_market)
-# 	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
-# 		ut_t.check_mock_file(mock_file, mock_json)
-# 		with pytest.raises(AssertionError) as assertion_message:
-# 			hyperparameter_config.HyperparameterConfigLoader.load('hyperparameter_config')
-# 		assert expected_message in str(assertion_message.value)
+# Test that checks that an invalid/broken config.json gets detected correctly
+@pytest.mark.parametrize('market_json, expected_message', invalid_values_testcases)
+def test_invalid_values(market_json, expected_message):
+	mock_json = json.dumps(market_json)
+	with patch('builtins.open', mock_open(read_data=mock_json)) as mock_file:
+		ut_t.check_mock_file(mock_file, mock_json)
+		with pytest.raises(AssertionError) as assertion_message:
+			HyperparameterConfigLoader.load('hyperparameter_config')
+		assert expected_message in str(assertion_message.value)
