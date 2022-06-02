fff9b47597ed5999fefc9444631489bc648656cb
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2022-06-02 13:12:41 +0200

    Renamed `market` to `sim_market`

--- recommerce/configuration/config_validation.py
+++ recommerce/configuration/config_validation.py
@@ -1,7 +1,6 @@
 # This file contains logic used by the webserver to validate configuration files
 
 from recommerce.configuration.environment_config import EnvironmentConfig
-from recommerce.configuration.hyperparameter_config import HyperparameterConfigValidator
 
 
 def validate_config(config: dict, config_is_final: bool) -> tuple:
@@ -39,41 +38,6 @@ def validate_config(config: dict, config_is_final: bool) -> tuple:
 		return False, str(error)
 
 
-def validate_sub_keys(config_class: HyperparameterConfigValidator or EnvironmentConfig, config: dict, top_level_keys: dict) -> None:
-	"""
-	Utility function that validates if a given config contains only allowed keys.
-	Can be used recursively for dictionaries within dictionaries.
-	"Unisex": Works for both HyperparameterConfigValidator and EnvironmentConfig.
-
-	Args:
-		config_class (HyperparameterConfigValidator or EnvironmentConfig): The config class from which to get the required fields.
-		config (dict): The config given by the user.
-		top_level_keys (dict): The keys of the current level. Their values indicate if there is another dictionary expected for that key.
-
-	Raises:
-		AssertionError: If the given config contains a key that is invalid.
-	"""
-	for key, _ in config.items():
-		# we need to separately check agents, since it is a list of dictionaries
-		if key == 'agents':
-			assert isinstance(config['agents'], list), f'The "agents" key must have a value of type list, but was {type(config["agents"])}'
-			for agent in config['agents']:
-				assert isinstance(agent, dict), f'All agents must be of type dict, but this one was {type(agent)}'
-				assert all(agent_key in {'name', 'agent_class', 'argument'} for agent_key in agent.keys()), \
-					f'An invalid key for agents was provided: {agent.keys()}'
-		# the key is key of a dictionary in the config
-		elif top_level_keys[key]:
-			assert isinstance(config[key], dict), f'The value of this key must be of type dict: {key}, but was {type(config[key])}'
-			# these are the valid keys that sub-key can have as keys in the dictionary
-			key_fields = config_class.get_required_fields(key)
-			# check that only valid keys were given by the user
-			for sub_key, _ in config[key].items():
-				assert sub_key in key_fields.keys(), \
-					f'The key "{sub_key}" should not exist within a {config_class.__name__} config (was checked at sub-key "{key}")'
-			# if there is an additional layer of dictionaries, check it recursively
-			validate_sub_keys(config_class, config[key], key_fields)
-
-
 def find_config_type(config: dict) -> str:
 	"""
 	Extract the config type from the config dictionary. Config type is defined by the "config_type" key, which must always be present.
@@ -88,47 +52,12 @@ def find_config_type(config: dict) -> str:
 		str: The config type.
 	"""
 	try:
-		if config['config_type'] in ['rl', 'market', 'environment']:
+		if config['config_type'] in ['rl', 'sim_market', 'environment']:
 			return config['config_type']
 		else:
-			raise AssertionError(f'the "config_type" key must be one of "rl", "market", "environment" but was {config["config_type"]}')
+			raise AssertionError(f'the "config_type" key must be one of "rl", "sim_market", "environment" but was {config["config_type"]}')
 	except KeyError as e:
-		raise AssertionError(f"your config is missing the 'config_type' key, which must be one of 'rl', 'market', 'environment': {config}") from e
-
-
-def split_mixed_config(config: dict) -> tuple:
-	"""
-	Utility function that splits a potentially mixed config of hyperparameters and environment-variables
-	into two dictionaries for the two configurations.
-
-	Args:
-		config (dict): The potentially mixed configuration.
-
-	Returns:
-		dict: The hyperparameter_config
-		dict: The environment_config
-
-	Raises:
-		AssertionError: If the user provides a key that should not exist.
-	"""
-	top_level_hyperparameter = HyperparameterConfigValidator.get_required_fields('top-dict')
-	top_level_environment = EnvironmentConfig.get_required_fields('top-dict')
-
-	hyperparameter_config = {}
-	environment_config = {}
-
-	for key, value in config.items():
-		if key in top_level_hyperparameter.keys():
-			hyperparameter_config[key] = value
-		elif key in top_level_environment.keys():
-			environment_config[key] = value
-		else:
-			raise AssertionError(f'Your config contains an invalid key: {key}')
-
-	validate_sub_keys(HyperparameterConfigValidator, hyperparameter_config, top_level_hyperparameter)
-	validate_sub_keys(EnvironmentConfig, environment_config, top_level_environment)
-
-	return hyperparameter_config, environment_config
+		raise AssertionError(f"your config is missing the 'config_type' key, must be one of 'rl', 'sim_market', 'environment': {config}") from e
 
 
 if __name__ == '__main__':  # pragma: no cover
@@ -144,7 +73,7 @@ if __name__ == '__main__':  # pragma: no cover
 		'epsilon_final': 0.1
 	}
 	test_config_market = {
-		'config_type': 'market',
+		'config_type': 'sim_market',
 		'max_storage': 100,
 		'episode_length': 50,
 		'max_price': 10,
