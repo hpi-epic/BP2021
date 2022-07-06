3a04eb12f2a408e5abd10ef38010c3303217b731
Judith <39854388+felix-20@users.noreply.github.com>
2022-07-06 11:31:41 +0200

    precommit!

--- docker_api/docker_manager.py
+++ docker_api/docker_manager.py
@@ -8,13 +8,12 @@ from datetime import datetime
 from itertools import count, filterfalse
 from types import GeneratorType
 
+import docker
 from container_db_manager import ContainerDB
 from docker.models.containers import Container
 from torch.cuda import is_available
 from utils import setup_logging
 
-# precommit please
-import docker
 
 IMAGE_NAME = 'recommerce'
 
