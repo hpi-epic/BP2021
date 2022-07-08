fc3acb87aef5ede738f35b0be3099c26db0936fa
Judith <39854388+felix-20@users.noreply.github.com>
2022-07-08 09:49:41 +0200

    bcolors back

--- docker/utils.py
+++ docker/utils.py
@@ -12,3 +12,15 @@ def setup_logging(name, level=logging.DEBUG):
 		level=level,
 		format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
 		datefmt='%Y-%m-%dT%H:%M:%S')
+
+
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKCYAN = '\033[96m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
